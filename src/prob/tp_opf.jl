2e28de4ae8baf0ef8109571882f822630c7baff6
carleton <cjc@lanl.gov>
2018-04-20 15:35:55 -0600

    updated tp_opf to support new lookup helper functions

--- src/prob/tp_opf.jl
+++ src/prob/tp_opf.jl
@@ -12,36 +12,34 @@ end
 
 ""
 function post_tp_opf(pm::GenericPowerModel)
-    for (n, nw_ref) in pm.ref[:nw]
-        for (h, ph_ref) in nw_ref[:ph]
-            PMs.variable_voltage(pm, n, h)
-            PMs.variable_generation(pm, n, h)
-            PMs.variable_branch_flow(pm, n, h)
-            PMs.variable_dcline_flow(pm, n, h)
+    for h in ph_ids(pm)
+        PMs.variable_voltage(pm, ph=h)
+        PMs.variable_generation(pm, ph=h)
+        PMs.variable_branch_flow(pm, ph=h)
+        PMs.variable_dcline_flow(pm, ph=h)
 
-            PMs.constraint_voltage(pm, n, h)
+        PMs.constraint_voltage(pm, ph=h)
 
-            for i in ids(pm, n, p, :ref_buses)
-                PMs.constraint_theta_ref(pm, n, h, i)
-            end
+        for i in ids(pm, :ref_buses, ph=h)
+            PMs.constraint_theta_ref(pm, i, ph=h)
+        end
 
-            for i in ids(pm, n, p, :bus)
-                PMs.constraint_kcl_shunt(pm, n, h, i)
-            end
+        for i in ids(pm, :bus, ph=h)
+            PMs.constraint_kcl_shunt(pm, i, ph=h)
+        end
 
-            for i in ids(pm, n, p, :branch)
-                PMs.constraint_ohms_yt_from(pm, n, h, i)
-                PMs.constraint_ohms_yt_to(pm, n, h, i)
+        for i in ids(pm, :branch, ph=h)
+            PMs.constraint_ohms_yt_from(pm, i, ph=h)
+            PMs.constraint_ohms_yt_to(pm, i, ph=h)
 
-                PMs.constraint_voltage_angle_difference(pm, n, h, i)
+            PMs.constraint_voltage_angle_difference(pm, i, ph=h)
 
-                PMs.constraint_thermal_limit_from(pm, n, h, i)
-                PMs.constraint_thermal_limit_to(pm, n, h, i)
-            end
+            PMs.constraint_thermal_limit_from(pm, i, ph=h)
+            PMs.constraint_thermal_limit_to(pm, i, ph=h)
+        end
 
-            for i in ids(pm, n, h, :dcline)
-                PMs.constraint_tp_dcline(pm, n, h, i)
-            end
+        for i in ids(pm, :dcline, ph=h)
+            PMs.constraint_tp_dcline(pm, i, ph=h)
         end
     end
 
