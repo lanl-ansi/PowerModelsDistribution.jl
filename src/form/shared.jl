70090846fc030edd97afb1a287e732bbee4207ea
David M Fobes <dfobes@lanl.gov>
2018-05-22 09:21:39 -0600

    UPD: Updated SOC constraint equations

--- src/form/shared.jl
+++ src/form/shared.jl
@@ -7,12 +7,12 @@ Creates Ohms constraints (yt post fix indicates that Y and T values are in recta
 function constraint_ohms_tp_yt_from(pm::GenericPowerModel{T}, n::Int, h::Int, f_bus, t_bus, f_idx, t_idx, g, b, g_fr, b_fr, tr, ti, tm) where T <: PMs.AbstractWRForms
     p_fr = var(pm, n, h, :p, f_idx)
     q_fr = var(pm, n, h, :q, f_idx)
-    w_fr = var(pm, n, h, :w, f_bus)
-    wr   = var(pm, n, h, :wr, (f_bus, t_bus))
-    wi   = var(pm, n, h, :wi, (f_bus, t_bus))
+    w_fr = [var(pm, n, j, :w, f_bus) for j in PMs.phase_ids(pm)]
+    wr   = [var(pm, n, j, :wr, (f_bus, t_bus)) for j in PMs.phase_ids(pm)]
+    wi   = [var(pm, n, j, :wi, (f_bus, t_bus)) for j in PMs.phase_ids(pm)]
 
-    @constraint(pm.model, p_fr ==  (g[h,h]+g_fr)/tm^2*w_fr + (-g*tr+b*ti)[h]/tm^2*wr + (-b*tr-g*ti)[h]/tm^2*wi )
-    @constraint(pm.model, q_fr == -(b[h,h]+b_fr)/tm^2*w_fr - (-b*tr-g*ti)[h]/tm^2*wr + (-g*tr+b*ti)[h]/tm^2*wi )
+    @constraint(pm.model, p_fr ==  g_fr[h]/tm[h]^2*w_fr[h] + sum(g[h,i]/tm[h]^2*w_fr[i] for i in PMs.phase_ids(pm)) + sum((-g[h,i]*tr[h]+b[h,i]*ti[h])/tm[h]^2*wr[i] + (-b[h,i]*tr[h]-g[h,i]*ti[h])/tm[h]^2*wi[i] for i in PMs.phase_ids(pm)) )
+    @constraint(pm.model, q_fr == -b_fr[h]/tm[h]^2*w_fr[h] - sum(b[h,i]/tm[h]^2*w_fr[i] for i in PMs.phase_ids(pm)) - sum((-b[h,i]*tr[h]-g[h,i]*ti[h])/tm[h]^2*wr[i] + (-g[h,i]*tr[h]+b[h,i]*ti[h])/tm[h]^2*wi[i] for i in PMs.phase_ids(pm)) )
 end
 
 
@@ -22,10 +22,15 @@ Creates Ohms constraints (yt post fix indicates that Y and T values are in recta
 function constraint_ohms_tp_yt_to(pm::GenericPowerModel{T}, n::Int, h::Int, f_bus, t_bus, f_idx, t_idx, g, b, g_to, b_to, tr, ti, tm) where T <: PMs.AbstractWRForms
     q_to = var(pm, n, h, :q, t_idx)
     p_to = var(pm, n, h, :p, t_idx)
-    w_to = var(pm, n, h, :w, t_bus)
-    wr   = var(pm, n, h, :wr, (f_bus, t_bus))
-    wi   = var(pm, n, h, :wi, (f_bus, t_bus))
+    w_to = [var(pm, n, j, :w, t_bus) for j in PMs.phase_ids(pm)]
+    wr   = [var(pm, n, j, :wr, (f_bus, t_bus)) for j in PMs.phase_ids(pm)]
+    wi   = [var(pm, n, j, :wi, (f_bus, t_bus)) for j in PMs.phase_ids(pm)]
 
-    @constraint(pm.model, p_to ==  (g[h,h]+g_to)*w_to + (-g*tr-b*ti)[h]/tm^2*wr + (-b*tr+g*ti)[h]/tm^2*-wi )
-    @constraint(pm.model, q_to == -(b[h,h]+b_to)*w_to - (-b*tr+g*ti)[h]/tm^2*wr + (-g*tr-b*ti)[h]/tm^2*-wi )
+    @constraint(pm.model, p_to ==  g_to[h]*w_to[h] + sum(g[h,i]*w_to[i] for i in PMs.phase_ids(pm)) + sum((-g[h,i]*tr[h]-b[h,i]*ti[h])/tm[h]^2*wr[i] + (-b[h,i]*tr[h]+g[h,i]*ti[h])/tm[h]^2*-wi[i] for i in PMs.phase_ids(pm)) )
+    @constraint(pm.model, q_to == -b_to[h]*w_to[h] - sum(b[h,i]*w_to[i] for i in PMs.phase_ids(pm)) - sum((-b[h,i]*tr[h]+g[h,i]*ti[h])/tm[h]^2*wr[i] + (-g[h,i]*tr[h]-b[h,i]*ti[h])/tm[h]^2*-wi[i] for i in PMs.phase_ids(pm)) )
+end
+
+
+"do nothing, no way to represent this in these variables"
+function constraint_tp_theta_ref(pm::GenericPowerModel{T}, n::Int, h::Int, i) where T <: PMs.AbstractWRForms
 end
--- src/form/wr.jl
+++ src/form/wr.jl
@@ -5,45 +5,6 @@
 Creates Ohms constraints (yt post fix indicates that Y and T values are in rectangular form)
 
 ```
-p[f_idx] == g/tm*w_fr_ne[i] + (-g*tr+b*ti)/tm*(wr_ne[i]) + (-b*tr-g*ti)/tm*(wi_ne[i])
-q[f_idx] == -(b+c/2)/tm*w_fr_ne[i] - (-b*tr-g*ti)/tm*(wr_ne[i]) + (-g*tr+b*ti)/tm*(wi_ne[i])
-```
-"""
-function constraint_ohms_tp_yt_from_ne(pm::GenericPowerModel{T}, n::Int, h::Int, i, f_bus, t_bus, f_idx, t_idx, g, b, g_fr, b_fr, tr, ti, tm, vad_min, vad_max) where T <: PMs.AbstractWRForm
-    p_fr = var(pm, n, h,    :p_ne, f_idx)
-    q_fr = var(pm, n, h,    :q_ne, f_idx)
-    w_fr = var(pm, n, h, :w_fr_ne, i)
-    wr   = var(pm, n, h,   :wr_ne, i)
-    wi   = var(pm, n, h,   :wi_ne, i)
-
-    @constraint(pm.model, p_fr ==  (g[h,h]+g_fr)/tm^2*w_fr + (-g*tr+b*ti)[h]/tm^2*wr + (-b*tr-g*ti)[h]/tm^2*wi )
-    @constraint(pm.model, q_fr == -(b[h,h]+b_fr)/tm^2*w_fr - (-b*tr-g*ti)[h]/tm^2*wr + (-g*tr+b*ti)[h]/tm^2*wi )
-end
-
-"""
-Creates Ohms constraints (yt post fix indicates that Y and T values are in rectangular form)
-
-```
-p[t_idx] == g*w_to_ne[i] + (-g*tr-b*ti)/tm*(wr_ne[i]) + (-b*tr+g*ti)/tm*(-wi_ne[i])
-q[t_idx] == -(b+c/2)*w_to_ne[i] - (-b*tr+g*ti)/tm*(wr_ne[i]) + (-g*tr-b*ti)/tm*(-wi_ne[i])
-```
-"""
-function constraint_ohms_tp_yt_to_ne(pm::GenericPowerModel{T}, n::Int, h::Int, i, f_bus, t_bus, f_idx, t_idx, g, b, g_to, b_to, tr, ti, tm, vad_min, vad_max) where T <: PMs.AbstractWRForm
-    p_to = var(pm, n, h,    :p_ne, t_idx)
-    q_to = var(pm, n, h,    :q_ne, t_idx)
-    w_to = var(pm, n, h, :w_to_ne, i)
-    wr   = var(pm, n, h,   :wr_ne, i)
-    wi   = var(pm, n, h,   :wi_ne, i)
-
-    @constraint(pm.model, p_to ==  (g[h,h]+g_to)*w_to + (-g*tr-b*ti)[h]/tm^2*wr + (-b*tr+g*ti)[h]/tm^2*-wi )
-    @constraint(pm.model, q_to == -(b[h,h]+b_to)*w_to - (-b*tr+g*ti)[h]/tm^2*wr + (-g*tr-b*ti)[h]/tm^2*-wi )
-end
-
-
-"""
-Creates Ohms constraints (yt post fix indicates that Y and T values are in rectangular form)
-
-```
 p[f_idx] ==        g/tm*w_fr[i] + (-g*tr+b*ti)/tm*(wr[i]) + (-b*tr-g*ti)/tm*(wi[i])
 q[f_idx] == -(b+c/2)/tm*w_fr[i] - (-b*tr-g*ti)/tm*(wr[i]) + (-g*tr+b*ti)/tm*(wi[i])
 ```
@@ -51,12 +12,13 @@ q[f_idx] == -(b+c/2)/tm*w_fr[i] - (-b*tr-g*ti)/tm*(wr[i]) + (-g*tr+b*ti)/tm*(wi[
 function constraint_ohms_tp_yt_from_on_off(pm::GenericPowerModel{T}, n::Int, h::Int, i, f_bus, t_bus, f_idx, t_idx, g, b, g_fr, b_fr, tr, ti, tm, vad_min, vad_max) where T <: PMs.AbstractWRForm
     p_fr = var(pm, n, h, :p, f_idx)
     q_fr = var(pm, n, h, :q, f_idx)
-    w_fr = var(pm, n, h, :w_fr, i)
-    wr   = var(pm, n, h, :wr, i)
-    wi   = var(pm, n, h, :wi, i)
+    w_fr = [var(pm, n, j, :w_fr, i) for j in PMs.phase_ids(pm)]
+    wr   = [var(pm, n, j, :wr, i) for j in PMs.phase_ids(pm)]
+    wi   = [var(pm, n, j, :wi, i) for j in PMs.phase_ids(pm)]
 
-    @constraint(pm.model, p_fr ==  (g[h,h]+g_fr)/tm^2*w_fr + (-g*tr+b*ti)[h]/tm^2*wr + (-b*tr-g*ti)[h]/tm^2*wi )
-    @constraint(pm.model, q_fr == -(b[h,h]+b_fr)/tm^2*w_fr - (-b*tr-g*ti)[h]/tm^2*wr + (-g*tr+b*ti)[h]/tm^2*wi )
+    @constraint(pm.model, p_fr ==  g_fr[h]/tm[h]^2*w_fr[h] + sum(g[h,i]/tm[h]^2*w_fr[i] for i in PMs.phase_ids(pm)) + sum((-g[h,i]*tr[h]+b[h,i]*ti[h])/tm[h]^2*wr[i] + (-b[h,i]*tr-g[h,i]*ti[h])/tm[h]^2*wi[i] for i in PMs.phase_ids(pm)) )
+    @constraint(pm.model, q_fr == -b_fr[h]/tm[h]^2*w_fr[h] - sum(g[h,i]/tm[h]^2*w_fr[i] for i in PMs.phase_ids(pm)) - sum((-b[h,i]*tr[h]-g[h,i]*ti[h])/tm[h]^2*wr[i] - (-g[h,i]*tr+b[h,i]*ti[h])/tm[h]^2*wi[i] for i in PMs.phase_ids(pm)) )
+
 end
 
 """
@@ -70,10 +32,10 @@ q[t_idx] == -(b+c/2)*w_to[i] - (-b*tr+g*ti)/tm*(wr[i]) + (-g*tr-b*ti)/tm*(-wi[i]
 function constraint_ohms_tp_yt_to_on_off(pm::GenericPowerModel{T}, n::Int, h::Int, i, f_bus, t_bus, f_idx, t_idx, g, b, g_to, b_to, tr, ti, tm, vad_min, vad_max) where T <: PMs.AbstractWRForm
     p_to = var(pm, n, h, :p, t_idx)
     q_to = var(pm, n, h, :q, t_idx)
-    w_to = var(pm, n, h, :w_to, i)
-    wr = var(pm, n, h, :wr, i)
-    wi = var(pm, n, h, :wi, i)
+    w_to = [var(pm, n, j, :w_to, i) for j in PMs.phase_ids(pm)]
+    wr   = [var(pm, n, j, :wr, i) for j in PMs.phase_ids(pm)]
+    wi   = [var(pm, n, j, :wi, i) for j in PMs.phase_ids(pm)]
 
-    @constraint(pm.model, p_to ==  (g[h,h]+g_to)*w_to + (-g*tr-b*ti)[h]/tm^2*wr + (-b*tr+g*ti)[h]/tm^2*-wi )
-    @constraint(pm.model, q_to == -(b[h,h]+b_to)*w_to - (-b*tr+g*ti)[h]/tm^2*wr + (-g*tr-b*ti)[h]/tm^2*-wi )
+    @constraint(pm.model, p_to ==  g_to[h]*w_to[h] + sum(g[h,i]*w_to[i] for i in PMs.phase_ids(pm)) + sum((-g[h,i]*tr[h]-b[h,i]*ti[h])/tm[h]^2*wr[i] + (-b[h,i]*tr[h]+g[h,i]*ti[h])/tm[h]^2*-wi[i] for i in PMs.phase_ids(pm)) )
+    @constraint(pm.model, q_to == -b_to[h]*w_to[h] - sum(b[h,i]*w_to[i] for i in PMs.phase_ids(pm)) - sum((-b[h,i]*tr[h]+g[h,i]*ti[h])/tm[h]^2*wr[i] + (-g[h,i]*tr[h]-b[h,i]*ti[h])/tm[h]^2*-wi[i] for i in PMs.phase_ids(pm)) )
 end
