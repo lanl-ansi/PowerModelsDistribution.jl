4356de204a67e6fed4511c6f8ba96292760e863f
David M Fobes <dfobes@lanl.gov>
2018-05-30 13:04:57 -0600

    REF: Prevent duplicate constraints in `constraint_tp_voltage`
    Ensures that there is no duplication of voltage constraints by changing
    to loop over `current_ph:last_phase` for each `current_phase` in
    `phase_ids(pm)`.
    
    Updates `on_off` ohms constraints

--- src/form/wr.jl
+++ src/form/wr.jl
@@ -14,7 +14,7 @@ function constraint_tp_voltage(pm::GenericPowerModel{T}, n::Int, h::Int) where T
     wr = var(pm, n, :wr)
     wi = var(pm, n, :wi)
 
-    for g in PMs.phase_ids(pm)
+    for g in h:length(PMs.phase_ids(pm))
         for (i,j) in ids(pm, n, :buspairs)
             InfrastructureModels.relaxation_complex_product(pm.model, w[(i,g)], w[(j,h)], wr[(i,j,h,g)], wi[(i,j,h,g)])
         end
@@ -39,13 +39,20 @@ q[f_idx] == -(b+c/2)/tm*w_fr[i] - (-b*tr-g*ti)/tm*(wr[i]) + (-g*tr+b*ti)/tm*(wi[
 function constraint_ohms_tp_yt_from_on_off(pm::GenericPowerModel{T}, n::Int, h::Int, i, f_bus, t_bus, f_idx, t_idx, g, b, g_fr, b_fr, tr, ti, tm, vad_min, vad_max) where T <: PMs.AbstractWRForm
     p_fr = var(pm, n, h, :p, f_idx)
     q_fr = var(pm, n, h, :q, f_idx)
-    w_fr = [var(pm, n, j, :w_fr, i) for j in PMs.phase_ids(pm)]
-    wr   = [var(pm, n, j, :wr, i) for j in PMs.phase_ids(pm)]
-    wi   = [var(pm, n, j, :wi, i) for j in PMs.phase_ids(pm)]
+    w    = var(pm, n, :w)
+    wr   = var(pm, n, :wr)
+    wi   = var(pm, n, :wi)
 
-    @constraint(pm.model, p_fr ==  g_fr[h]/tm[h]^2*w_fr[h] + sum(g[h,i]/tm[h]^2*w_fr[i] for i in PMs.phase_ids(pm)) + sum((-g[h,i]*tr[h]+b[h,i]*ti[h])/tm[h]^2*wr[i] + (-b[h,i]*tr-g[h,i]*ti[h])/tm[h]^2*wi[i] for i in PMs.phase_ids(pm)) )
-    @constraint(pm.model, q_fr == -b_fr[h]/tm[h]^2*w_fr[h] - sum(g[h,i]/tm[h]^2*w_fr[i] for i in PMs.phase_ids(pm)) - sum((-b[h,i]*tr[h]-g[h,i]*ti[h])/tm[h]^2*wr[i] - (-g[h,i]*tr+b[h,i]*ti[h])/tm[h]^2*wi[i] for i in PMs.phase_ids(pm)) )
-
+    @constraint(pm.model, p_fr ==  ( g_fr[h]+g[h,h]) * w[(f_bus, h)] +
+                                sum( g[h,i] * wr[(f_bus, f_bus, h, i)] +
+                                     b[h,i] * wi[(f_bus, f_bus, h, i)] for i in PMs.phase_ids(pm) if i != h) +
+                                sum(-g[h,i] * wr[(f_bus, t_bus, h, i)] +
+                                    -b[h,i] * wi[(f_bus, t_bus, h, i)] for i in PMs.phase_ids(pm)) )
+    @constraint(pm.model, q_fr == -( b_fr[h]+b[h,h]) * w[(f_bus, h)] -
+                                sum( b[h,i] * wr[(f_bus, f_bus, h, i)] -
+                                     g[h,i] * wi[(f_bus, f_bus, h, i)] for i in PMs.phase_ids(pm) if i != h) -
+                                sum(-b[h,i] * wr[(f_bus, t_bus, h, i)] +
+                                     g[h,i] * wi[(f_bus, t_bus, h, i)] for i in PMs.phase_ids(pm)) )
 end
 
 """
@@ -59,10 +66,18 @@ q[t_idx] == -(b+c/2)*w_to[i] - (-b*tr+g*ti)/tm*(wr[i]) + (-g*tr-b*ti)/tm*(-wi[i]
 function constraint_ohms_tp_yt_to_on_off(pm::GenericPowerModel{T}, n::Int, h::Int, i, f_bus, t_bus, f_idx, t_idx, g, b, g_to, b_to, tr, ti, tm, vad_min, vad_max) where T <: PMs.AbstractWRForm
     p_to = var(pm, n, h, :p, t_idx)
     q_to = var(pm, n, h, :q, t_idx)
-    w_to = [var(pm, n, j, :w_to, i) for j in PMs.phase_ids(pm)]
-    wr   = [var(pm, n, j, :wr, i) for j in PMs.phase_ids(pm)]
-    wi   = [var(pm, n, j, :wi, i) for j in PMs.phase_ids(pm)]
+    w    = var(pm, n, :w)
+    wr   = var(pm, n, :wr)
+    wi   = var(pm, n, :wi)
 
-    @constraint(pm.model, p_to ==  g_to[h]*w_to[h] + sum(g[h,i]*w_to[i] for i in PMs.phase_ids(pm)) + sum((-g[h,i]*tr[h]-b[h,i]*ti[h])/tm[h]^2*wr[i] + (-b[h,i]*tr[h]+g[h,i]*ti[h])/tm[h]^2*-wi[i] for i in PMs.phase_ids(pm)) )
-    @constraint(pm.model, q_to == -b_to[h]*w_to[h] - sum(b[h,i]*w_to[i] for i in PMs.phase_ids(pm)) - sum((-b[h,i]*tr[h]+g[h,i]*ti[h])/tm[h]^2*wr[i] + (-g[h,i]*tr[h]-b[h,i]*ti[h])/tm[h]^2*-wi[i] for i in PMs.phase_ids(pm)) )
+    @constraint(pm.model, p_to ==  ( g_to[h]+g[h,h]) * w[(t_bus, h)] +
+                                sum( g[h,i] * wr[(t_bus, t_bus, h, i)] +
+                                     b[h,i] *-wi[(t_bus, t_bus, h, i)] for i in PMs.phase_ids(pm) if i != h) +
+                                sum(-g[h,i] * wr[(f_bus, t_bus, h, i)] +
+                                    -b[h,i] *-wi[(f_bus, t_bus, h, i)] for i in PMs.phase_ids(pm)) )
+    @constraint(pm.model, q_to == -( b_to[h]+b[h,h]) * w[(t_bus, h)] -
+                                sum( b[h,i] * wr[(t_bus, t_bus, h, i)] -
+                                     g[h,i] *-wi[(t_bus, t_bus, h, i)] for i in PMs.phase_ids(pm) if i != h) -
+                                sum(-b[h,i] * wr[(f_bus, t_bus, h, i)] +
+                                     g[h,i] *-wi[(f_bus, t_bus, h, i)] for i in PMs.phase_ids(pm)) )
 end
