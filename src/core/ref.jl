456d3d6572871c4c2c0a13468902584bd9028dfd
David M Fobes <dfobes@lanl.gov>
2018-06-01 08:40:13 -0600

    FIX: Eliminate usage of `getmpv`
    Eliminates the usage of `getmpv` from PowerModels in
    `core/ref.jl:calc_tp_voltage_product_bounds`.

--- src/core/ref.jl
+++ src/core/ref.jl
@@ -8,22 +8,22 @@ function calc_tp_voltage_product_bounds(pm::GenericPowerModel, buspairs; nw::Int
     for (i, j, h, g) in buspairs
         if i == j
             bus = ref(pm, nw, :bus)[i]
-            vm_fr_max = getmpv(bus["vmax"], h)
-            vm_to_max = getmpv(bus["vmax"], g)
-            vm_fr_min = getmpv(bus["vmin"], h)
-            vm_to_min = getmpv(bus["vmin"], g)
+            vm_fr_max = bus["vmax"][h]
+            vm_to_max = bus["vmax"][g]
+            vm_fr_min = bus["vmin"][h]
+            vm_to_min = bus["vmin"][g]
         else
             buspair = ref(pm, nw, :buspairs)[(i, j)]
-            vm_fr_max = getmpv(buspair["vm_fr_max"], h)
-            vm_to_max = getmpv(buspair["vm_to_max"], g)
-            vm_fr_min = getmpv(buspair["vm_fr_min"], h)
-            vm_to_min = getmpv(buspair["vm_to_min"], g)
+            vm_fr_max = buspair["vm_fr_max"][h]
+            vm_to_max = buspair["vm_to_max"][g]
+            vm_fr_min = buspair["vm_fr_min"][h]
+            vm_to_min = buspair["vm_to_min"][g]
         end
 
-        wr_max[(i, j, h, g)] =  getmpv(vm_fr_max, h) * getmpv(vm_to_max, g)
-        wr_min[(i, j, h, g)] = -getmpv(vm_fr_max, h) * getmpv(vm_to_max, g)
-        wi_max[(i, j, h, g)] =  getmpv(vm_fr_max, h) * getmpv(vm_to_max, g)
-        wi_min[(i, j, h, g)] = -getmpv(vm_fr_max, h) * getmpv(vm_to_max, g)
+        wr_max[(i, j, h, g)] =  vm_fr_max * vm_to_max
+        wr_min[(i, j, h, g)] = -vm_fr_max * vm_to_max
+        wi_max[(i, j, h, g)] =  vm_fr_max * vm_to_max
+        wi_min[(i, j, h, g)] = -vm_fr_max * vm_to_max
     end
 
     return wr_min, wr_max, wi_min, wi_max
