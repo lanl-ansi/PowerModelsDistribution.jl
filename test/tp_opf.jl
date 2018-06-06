5c94e3d182ee9c57fe3351225a3f49230f4ba733
David M Fobes <dfobes@lanl.gov>
2018-06-06 13:50:13 -0600

    ADD: OPF tests for coupled networks
    Adds test cases of coupled networks for OPF problem.
    
    * coupled meshed
    * coupled radial no shunts
    * coupled radial shunts

--- test/tp_opf.jl
+++ test/tp_opf.jl
@@ -108,32 +108,56 @@ end
             @test isapprox(result["solution"]["bus"]["2"]["va"][ph], -0.0135651+2*pi/mp_data["phases"]*(ph-1); atol = 1e-3)
         end
     end
-    @testset "5-bus independent meshed different off-diagonal ac case" begin
-        mp_data = ThreePhasePowerModels.parse_file("../test/data/matlab/case5_i_m_c.m")
+    @testset "5-bus coupled meshed case" begin
+        @testset "ac case" begin
+            mp_data = ThreePhasePowerModels.parse_file("../test/data/matlab/case5_c_m_a.m")
+            result = run_tp_opf(mp_data, PMs.ACPPowerModel, ipopt_solver)
+
+            @test result["status"] == :LocalOptimal
+            @test isapprox(result["objective"], 53350.44; atol = 1e-1)
+
+            for ph in 1:mp_data["phases"]
+                @test isapprox(result["solution"]["gen"]["1"]["qg"][ph],  0.29999999; atol = 1e-3)
+            end
+            @test isapprox(result["solution"]["bus"]["2"]["va"][1], -0.0135573; atol = 1e-3)
+            @test isapprox(result["solution"]["bus"]["2"]["va"][2], -0.0123172+2*pi/mp_data["phases"]; atol = 1e-3)
+            @test isapprox(result["solution"]["bus"]["2"]["va"][3], -0.0136547+4*pi/mp_data["phases"]; atol = 1e-3)
+        end
+        @testset "soc case" begin
+            mp_data = ThreePhasePowerModels.parse_file("../test/data/matlab/case5_c_m_a.m")
+            result = run_tp_opf(mp_data, PMs.SOCWRPowerModel, ipopt_solver)
+
+            @test result["status"] == :LocalOptimal
+            @test isapprox(result["objective"], 21553.9; atol = 1e-1)
+
+            @test isapprox(result["solution"]["gen"]["1"]["qg"][1], -0.037881; atol = 1e-3)
+            @test isapprox(result["solution"]["gen"]["1"]["qg"][2], -0.299998; atol = 1e-3)
+            @test isapprox(result["solution"]["gen"]["1"]["qg"][3], -0.075929; atol = 1e-3)
+        end
+    end
+    @testset "5-bus coupled radial no shunt case" begin
+        mp_data = ThreePhasePowerModels.parse_file("../test/data/matlab/case5_c_r_a.m")
         result = run_tp_opf(mp_data, PMs.ACPPowerModel, ipopt_solver)
 
         @test result["status"] == :LocalOptimal
-        @test isapprox(result["objective"], 53350.44; atol = 1e-1)
+        @test isapprox(result["objective"], 55448.1; atol = 1e-1)
 
         for ph in 1:mp_data["phases"]
-            @test isapprox(result["solution"]["gen"]["1"]["qg"][ph],  0.29999999; atol = 1e-3)
+            @test isapprox(result["solution"]["gen"]["1"]["pg"][ph], 0.4; atol = 1e-3)
+            @test isapprox(result["solution"]["bus"]["2"]["vm"][ph], 1.08564; atol = 1e-3)
+            @test isapprox(result["solution"]["bus"]["2"]["va"][ph], 0.04905+2*pi/mp_data["phases"]*(ph-1); atol = 1e-3)
         end
-        @test isapprox(result["solution"]["bus"]["2"]["va"][1], -0.0135573; atol = 1e-3)
-        @test isapprox(result["solution"]["bus"]["2"]["va"][2], -0.0123172+2*pi/mp_data["phases"]; atol = 1e-3)
-        @test isapprox(result["solution"]["bus"]["2"]["va"][3], -0.0136547+4*pi/mp_data["phases"]; atol = 1e-3)
-
     end
-    @testset "5-bus independent meshed different off-diagonal soc case" begin
-        mp_data = ThreePhasePowerModels.parse_file("../test/data/matlab/case5_i_m_c.m")
-        result = run_tp_opf(mp_data, PMs.SOCWRPowerModel, ipopt_solver)
+    @testset "5-bus coupled radial shunt case" begin
+        mp_data = ThreePhasePowerModels.parse_file("../test/data/matlab/case5_c_r_b.m")
+        result = run_tp_opf(mp_data, PMs.ACPPowerModel, ipopt_solver)
 
         @test result["status"] == :LocalOptimal
-        # @test isapprox(result["objective"], 28233.4518; atol = 1e-1)
+        @test isapprox(result["objective"], 56087.4; atol = 1e-1)
 
-        # @test isapprox(result["solution"]["gen"]["1"]["qg"][1], -0.003862; atol = 1e-3)
-        # @test isapprox(result["solution"]["gen"]["1"]["qg"][2], -0.098937; atol = 1e-3)
-        # @test isapprox(result["solution"]["gen"]["1"]["qg"][3], -0.003651; atol = 1e-3)
-
+        for ph in 1:mp_data["phases"]
+            @test isapprox(result["solution"]["gen"]["1"]["pg"][ph],  0.4; atol = 1e-3)
+            @test isapprox(result["solution"]["bus"]["2"]["va"][ph], 0.055338+2*pi/mp_data["phases"]*(ph-1); atol = 5e-3)
+        end
     end
-
 end
