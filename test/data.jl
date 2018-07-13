8c6918d8a640c34052adbf0d9f676a896d927aab
Frederik <frederik.geth@gmail.com>
2018-07-14 09:32:40 +1000

    Change of phase order at reference bus (#52)
    * Change of phase order + add fixed voltage magnitude bus support
    * fixes per discussion in PR TPPM

--- test/data.jl
+++ test/data.jl
@@ -8,3 +8,16 @@
         @test InfrastructureModels.compare_dict(data, data_base)
     end
 end
+
+
+@testset "test angle wrapper functions" begin
+    @testset "test wraptopi" begin
+        wrappedradians = ThreePhasePowerModels.wraptopi([0, pi/2, pi, 3pi/2, 2pi])
+        @test isapprox(wrappedradians, [0, pi/2, -pi, -pi/2, 0]; atol=1e-12)
+    end
+
+    @testset "test wrapto180" begin
+        wrappeddegrees = ThreePhasePowerModels.wrapto180([0, 90, 180, 270, 360])
+        @test isapprox(wrappeddegrees, [0, 90, -180, -90, 0]; atol=1e-12)
+    end
+end
