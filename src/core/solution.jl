360938c5b391591aa4bc778d508a7f823748dc8f
David M Fobes <dfobes@lanl.gov>
2019-04-29 11:16:01 -0600

    UPD: solution.jl

--- src/core/solution.jl
+++ src/core/solution.jl
@@ -1,5 +1,5 @@
 "Adds sequence components of the voltage to the solution dict."
-function get_solution_vseq(pm::GenericPowerModel, sol::Dict{String,<:Any})
+function get_solution_vseq(pm::PMs.GenericPowerModel, sol::Dict{String,<:Any})
     PMs.get_solution(pm, sol)
     if !ismultinetwork(pm)
         get_solution_vseq(pm, pm.cnw, sol)
@@ -10,10 +10,10 @@ function get_solution_vseq(pm::GenericPowerModel, sol::Dict{String,<:Any})
     end
 end
 
-function get_solution_vseq(pm::GenericPowerModel, nw::Int, sol_nw::Dict{String,<:Any})
+function get_solution_vseq(pm::PMs.GenericPowerModel, nw::Int, sol_nw::Dict{String,<:Any})
     PMs.get_solution(pm, sol_nw)
-    if !ismulticonductor(pm, nw=nw) && ref(pm, nw, :conductors)==3
-        error(LOGGER, "Sequence components are only defined on three-phase networks.")
+    if !ismulticonductor(pm, nw=nw) && PMs.ref(pm, nw, :conductors)==3
+        Memento.error(LOGGER, "Sequence components are only defined on three-phase networks.")
     end
     for bus_id in PMs.ids(pm, nw, :bus)
         vabc = [sol_nw["bus"]["$bus_id"]["vm"][c]*exp(im*sol_nw["bus"]["$bus_id"]["va"][c]) for c in 1:3]
