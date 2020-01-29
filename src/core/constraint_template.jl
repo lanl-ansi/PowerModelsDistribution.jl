8ec7d10e417a0ddba99b74c24527ea755e9e8ede
Sander Claeys <id@sanderclaeys.eu>
2020-01-28 17:02:32 -0700

    fixed power_balance_shed

--- src/core/constraint_template.jl
+++ src/core/constraint_template.jl
@@ -392,31 +392,29 @@ end
 
 "KCL for load shed problem with transformers"
 function constraint_mc_power_balance_shed(pm::_PMs.AbstractPowerModel, i::Int; nw::Int=pm.cnw)
-    for cnd in _PMs.conductor_ids(pm; nw=nw)
-        if !haskey(_PMs.con(pm, nw), :kcl_p)
-            _PMs.con(pm, nw)[:kcl_p] = Dict{Int,Array{JuMP.ConstraintRef,1}}()
-        end
-        if !haskey(_PMs.con(pm, nw), :kcl_q)
-            _PMs.con(pm, nw)[:kcl_q] = Dict{Int,Array{JuMP.ConstraintRef,1}}()
-        end
-
-        bus = _PMs.ref(pm, nw, :bus, i)
-        bus_arcs = _PMs.ref(pm, nw, :bus_arcs, i)
-        bus_arcs_sw = _PMs.ref(pm, nw, :bus_arcs_sw, i)
-        bus_arcs_trans = _PMs.ref(pm, nw, :bus_arcs_trans, i)
-        bus_gens = _PMs.ref(pm, nw, :bus_gens, i)
-        bus_storage = _PMs.ref(pm, nw, :bus_storage, i)
-        bus_loads = _PMs.ref(pm, nw, :bus_loads, i)
-        bus_shunts = _PMs.ref(pm, nw, :bus_shunts, i)
-
-        bus_pd = Dict(k => _PMs.ref(pm, nw, :load, k, "pd") for k in bus_loads)
-        bus_qd = Dict(k => _PMs.ref(pm, nw, :load, k, "qd") for k in bus_loads)
-
-        bus_gs = Dict(k => _PMs.ref(pm, nw, :shunt, k, "gs") for k in bus_shunts)
-        bus_bs = Dict(k => _PMs.ref(pm, nw, :shunt, k, "bs") for k in bus_shunts)
-
-        constraint_mc_power_balance_shed(pm, nw, i, bus_arcs, bus_arcs_sw, bus_arcs_trans, bus_gens, bus_storage, bus_pd, bus_qd, bus_gs, bus_bs)
+    if !haskey(_PMs.con(pm, nw), :kcl_p)
+        _PMs.con(pm, nw)[:kcl_p] = Dict{Int,Array{JuMP.ConstraintRef,1}}()
     end
+    if !haskey(_PMs.con(pm, nw), :kcl_q)
+        _PMs.con(pm, nw)[:kcl_q] = Dict{Int,Array{JuMP.ConstraintRef,1}}()
+    end
+
+    bus = _PMs.ref(pm, nw, :bus, i)
+    bus_arcs = _PMs.ref(pm, nw, :bus_arcs, i)
+    bus_arcs_sw = _PMs.ref(pm, nw, :bus_arcs_sw, i)
+    bus_arcs_trans = _PMs.ref(pm, nw, :bus_arcs_trans, i)
+    bus_gens = _PMs.ref(pm, nw, :bus_gens, i)
+    bus_storage = _PMs.ref(pm, nw, :bus_storage, i)
+    bus_loads = _PMs.ref(pm, nw, :bus_loads, i)
+    bus_shunts = _PMs.ref(pm, nw, :bus_shunts, i)
+
+    bus_pd = Dict(k => _PMs.ref(pm, nw, :load, k, "pd") for k in bus_loads)
+    bus_qd = Dict(k => _PMs.ref(pm, nw, :load, k, "qd") for k in bus_loads)
+
+    bus_gs = Dict(k => _PMs.ref(pm, nw, :shunt, k, "gs") for k in bus_shunts)
+    bus_bs = Dict(k => _PMs.ref(pm, nw, :shunt, k, "bs") for k in bus_shunts)
+
+    constraint_mc_power_balance_shed(pm, nw, i, bus_arcs, bus_arcs_sw, bus_arcs_trans, bus_gens, bus_storage, bus_pd, bus_qd, bus_gs, bus_bs)
 end
 
 
