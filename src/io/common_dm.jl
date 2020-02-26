b2efa2d03100c4eebfdf48ef762e0560feade366
David M Fobes <dfobes@lanl.gov>
2020-02-26 16:26:44 -0700

    parse_file_dm rewrite

--- src/io/common_dm.jl
+++ src/io/common_dm.jl
@@ -3,12 +3,12 @@
 
 Parses the IOStream of a file into a Three-Phase PowerModels data structure.
 """
-function parse_file_dm(io::IO; import_all::Bool=false, vmin::Float64=0.9, vmax::Float64=1.1, filetype::AbstractString="json", bank_transformers::Bool=true)
+function parse_file_dm(io::IO; import_all::Bool=false, filetype::AbstractString="json", bank_transformers::Bool=true)
     if filetype == "m"
         pmd_data = PowerModelsDistribution.parse_matlab(io)
     elseif filetype == "dss"
         Memento.warn(_LOGGER, "Not all OpenDSS features are supported, currently only minimal support for lines, loads, generators, and capacitors as shunts. Transformers and reactors as transformer branches are included, but value translation is not fully supported.")
-        pmd_data = PowerModelsDistribution.parse_opendss_dm(io; import_all=import_all, vmin=vmin, vmax=vmax, bank_transformers=bank_transformers)
+        pmd_data = PowerModelsDistribution.parse_opendss_dm(io; import_all=import_all, bank_transformers=bank_transformers)
     elseif filetype == "json"
         pmd_data = PowerModels.parse_json(io; validate=false)
     else
@@ -22,20 +22,36 @@ end
 
 
 ""
-function parse_file_dm(file::String; kwargs...)
+function parse_file_dm(file::String; model::String="engineering", kwargs...)
     pmd_data = open(file) do io
         parse_file_dm(io; filetype=split(lowercase(file), '.')[end], kwargs...)
     end
 
-    # to get the old indexing
-    pmd_data_old = open(file) do io
-        parse_file(io; filetype=split(lowercase(file), '.')[end], kwargs...)
-    end
-    index_presets = Dict(comp_type=>Dict(comp["name"]=>comp["index"] for (id, comp) in pmd_data_old[comp_type]) for comp_type in ["bus"])
+    if model == "mathematical"
+        # to get the old indexing
+        pmd_data_old = open(file) do io
+            parse_file(io; filetype=split(lowercase(file), '.')[end], kwargs...)
+        end
+        index_presets = Dict(comp_type=>Dict(comp["name"]=>comp["index"] for (id, comp) in pmd_data_old[comp_type]) for comp_type in ["bus"])
 
-    data_model_map!(pmd_data)
-    data_model_make_pu!(pmd_data)
-    data_model_index!(pmd_data; index_presets=index_presets)
-    data_model_make_compatible_v8!(pmd_data)
+        transform_model!(pmd_data; index_presets=index_presets)
+    end
+
     return pmd_data
+end
+
+
+"transforms model between engineering (high-level) and mathematical (low-level) models"
+function transform_data_model!(data::Dict{String,Any}; index_presets::Dict{String,<:Any}=Dict{String,Any}())
+    if get(data, "model", "mathematical") == "engineering"
+        data_model_map!(data)
+        data_model_make_pu!(data)
+        data_model_index!(data, index_presets=index_presets)
+        data_model_make_compatible_v8!(data)
+    else
+        if haskey(data, "")
+        else
+            Memento.warn(_LOGGER, "Cannot transform mathematical model to engineering model, no mapping information available")
+        end
+    end
 end
