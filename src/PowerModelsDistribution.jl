3f95e9669c6e02737512330be961e24de355490a
David M Fobes <dfobes@lanl.gov>
2020-03-03 09:22:46 -0700

    FIX: duplicate includes

--- src/PowerModelsDistribution.jl
+++ src/PowerModelsDistribution.jl
@@ -42,13 +42,10 @@ module PowerModelsDistribution
     include("io/common_dm.jl")
     include("io/opendss_dm.jl")
 
-    include("io/common.jl")
     include("io/json.jl")
     include("io/dss_parse.jl")
     include("io/dss_structs.jl")
 
-    include("io/common_dm.jl")
-    include("io/opendss_dm.jl")
     include("io/data_model_components.jl")
     include("core/data_model_mapping.jl")
     include("core/data_model_pu.jl")
