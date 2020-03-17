c1ed2d3251813b8ddb61f8912ac9ab7f8506c2a5
David M Fobes <dfobes@lanl.gov>
2020-03-17 15:19:01 -0600

    WIP: ADD math2eng conversions

--- src/data_model/math2eng.jl
+++ src/data_model/math2eng.jl
@@ -3,13 +3,15 @@ function _map_math2eng!(data_math)
     @assert get(data_math, "data_model", "mathematical") == "mathematical" "Cannot map data to engineering model: provided data is not a mathematical model"
     @assert haskey(data_math, "map") "Cannot map data to engineering model: no mapping from mathematical to engineering data model is provided"
 
-    data_eng = Dict{<:Any,<:Any}()
+    data_eng = Dict{String,Any}()
 
-    map_keys = sort(keys(data_math["map"]); rev=true)
-    for map in map_keys
-        # TODO
+    unmap_keys = sort(keys(data_math["map"]), rev=true)
+    reverse_bus_lookup = Dict{Int,Any}((bus_math, bus_eng) for (bus_eng, bus_math) in data_math["bus_lookup"])
+    for key in unmap_keys
+        @eval $(map[:unmap_function])(data_eng, data_math, data_math["map"][key], reverse_bus_lookup)
     end
 
+    return data_eng
 end
 
 
@@ -63,4 +65,148 @@ function solution_math2eng(solution_math::Dict, data_math::Dict; make_si::Bool=t
     end
 
     return solution_eng
+end
+
+
+function _map_math2eng_voltage_source!(data_eng::Dict{String,<:Any}, data_math::Dict{String,<:Any}, map::Dict{Symbol,<:Any}, bus_lookup::Dict{Int,<:Any})
+    if !haskey(data_eng, "voltage_source")
+        data_eng["voltage_source"] = Dict{Any,Any}()
+    end
+
+    eng_obj = Dict{String,Any}()
+    merge!(eng_obj, map[:extras])
+
+    for to_id in map[:to]
+        math_obj = _get_math_obj(data_math, to_id)
+        if startswith(to_id, "bus")
+            eng_obj["vm"] = math_obj["vm"]
+            eng_obj["va"] = math_obj["va"]
+
+        elseif startswith(to_id, "gen")
+            eng_obj["source_id"] = strip(math_obj["source_id"], "_virtual_gen.")
+
+        elseif startswith(to_id, "branch")
+            eng_obj["bus"] = bus_lookup[math_obj["t_bus"]]
+            eng_obj["rs"] = math_obj["br_r"]
+            eng_obj["xs"] = math_obj["br_x"]
+        else
+            Memento.warn(_LOGGER, "transforming from $to_id to $(map[:from]) is not supported")
+        end
+    end
+
+    data_eng[map[:from]] = eng_obj
+end
+
+
+function _map_math2eng_bus!(data_eng::Dict{String,<:Any}, data_math::Dict{String,<:Any}, map::Dict{Symbol,<:Any}, bus_lookup::Dict{Int,<:Any})
+    eng_obj = _init_unmap_eng_obj!(data_eng, "bus", map)
+    math_obj = _get_math_obj(data_math, map[:to])
+
+    eng_obj["status"] = math_obj["bus_type"] == 4 ? 0 : 1
+
+    for key in ["vm", "va", "vmin", "vmax"]
+        if haskey(math_obj, key)
+            eng_obj[key] = math_obj[key]
+        end
+    end
+
+    data_eng[map[:from]] = eng_obj
+end
+
+
+function _map_math2eng_load!(data_eng::Dict{String,<:Any}, data_math::Dict{String,<:Any}, map::Dict{Symbol,<:Any}, bus_lookup::Dict{Int,<:Any})
+    eng_obj = _init_unmap_eng_obj!(data_eng, "load", map)
+    math_obj = _get_math_obj(data_math, map[:to])
+
+    eng_obj["vnom"] = math_obj["vnom_kv"]
+    eng_obj["bus"] = bus_lookup[math_obj["load_bus"]]
+
+    eng_obj["pd"] = math_obj["pd"]
+    eng_obj["qd"] = math_obj["qd"]
+
+    eng_obj["status"] = math_obj["status"]
+
+    eng_obj["configuration"] = math_obj["configuration"]
+
+    data_eng[map[:from]] = eng_obj
+end
+
+
+function _map_math2eng_shunt_capacitor!(data_eng::Dict{String,<:Any}, data_math::Dict{String,<:Any}, map::Dict{Symbol,<:Any}, bus_lookup::Dict{Int,<:Any})
+    eng_obj = _init_unmap_eng_obj!(data_eng, "shunt_capacitor", map)
+    math_obj = _get_math_obj(data_math, map[:to])
+
+    eng_obj["bus"] = bus_lookup["shunt_bus"]
+    eng_obj["status"] = math_obj["status"]
+
+    eng_obj["configuration"] = eng_obj["configuration"]
+
+    B = math_obj["bs"]
+
+    if math_obj["configuration"] == "wye"
+        b_cap_pu = diag(B)
+    else
+        # TODO
+    end
+
+    # TODO how to pull kv, kvar back out, this is a destructive transformation
+
+    data_eng[map[:from]] = eng_obj
+end
+
+
+function _map_math2eng_shunt!(data_eng::Dict{String,<:Any}, data_math::Dict{String,<:Any}, map::Dict{Symbol,<:Any}, bus_lookup::Dict{Int,<:Any})
+end
+
+
+function _map_math2eng_generator!(data_eng::Dict{String,<:Any}, data_math::Dict{String,<:Any}, map::Dict{Symbol,<:Any}, bus_lookup::Dict{Int,<:Any})
+    eng_obj = _init_unmap_eng_obj!(data_eng, "generator", map)
+    math_obj = _get_math_obj(data_math, map[:to])
+
+    eng_obj["bus"] = bus_lookup[math_obj["gen_bus"]]
+    eng_obj["configuration"] = math_obj["configuration"]
+    eng_obj["status"] = math_obj["gen_status"]
+
+    eng_obj["kw"] = math_obj["pg"]
+    eng_obj["kvar"] = math_obj["qg"]
+    eng_obj["kv"] = math_obj["vg"]
+
+    eng_obj["kvar_min"] = math_obj["qmin"]
+    eng_obj["kvar_max"] = math_obj["qmax"]
+
+    eng_obj["kw_min"] = math_obj["pmin"]
+    eng_obj["kw_max"] = math_obj["pmax"]
+
+    gen_bus = data_math["bus"]["$(math_obj["gen_bus"])"]
+    if gen_bus["bus_type"] == 2
+        eng_obj["control_mode"] = 3
+    else
+        eng_obj["control_mode"] = 1
+    end
+
+    data_eng[map[:from]] = eng_obj
+end
+
+
+function _map_math2eng_solar!(data_eng::Dict{String,<:Any}, data_math::Dict{String,<:Any}, map::Dict{Symbol,<:Any}, bus_lookup::Dict{Int,<:Any})
+end
+
+
+function _map_math2eng_storage!(data_eng::Dict{String,<:Any}, data_math::Dict{String,<:Any}, map::Dict{Symbol,<:Any}, bus_lookup::Dict{Int,<:Any})
+end
+
+
+function _map_math2eng_line!(data_eng::Dict{String,<:Any}, data_math::Dict{String,<:Any}, map::Dict{Symbol,<:Any}, bus_lookup::Dict{Int,<:Any})
+end
+
+
+function _map_math2eng_line_reactor!(data_eng::Dict{String,<:Any}, data_math::Dict{String,<:Any}, map::Dict{Symbol,<:Any}, bus_lookup::Dict{Int,<:Any})
+end
+
+
+function _map_math2eng_switch!(data_eng::Dict{String,<:Any}, data_math::Dict{String,<:Any}, map::Dict{Symbol,<:Any}, bus_lookup::Dict{Int,<:Any})
+end
+
+
+function _map_math2eng_transformer!(data_eng::Dict{String,<:Any}, data_math::Dict{String,<:Any}, map::Dict{Symbol,<:Any}, bus_lookup::Dict{Int,<:Any})
 end
