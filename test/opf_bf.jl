0304d691cf7223fcd16df7e54edd7a1ec664556c
David M Fobes <dfobes@lanl.gov>
2020-04-21 15:04:13 -0600

    REF: opf_bf tests to eng data model

--- test/opf_bf.jl
+++ test/opf_bf.jl
@@ -1,13 +1,14 @@
 @info "running branch-flow optimal power flow (opf_bf) tests"
 
 @testset "test distflow formulations" begin
+    case5 = PowerModels.parse_file("../test/data/matpower/case5.m")
+    make_multiconductor!(case5, 3)
+
     @testset "test linearised distflow opf_bf" begin
         @testset "5-bus lpubfdiag opf_bf" begin
-            mp_data = PowerModels.parse_file("../test/data/matpower/case5.m")
-            PMD.make_multiconductor!(mp_data, 3)
-            result = run_mc_opf_bf(mp_data, LPUBFDiagPowerModel, ipopt_solver)
+            result = run_mc_opf_bf(case5, LPUBFDiagPowerModel, ipopt_solver)
 
-            @test result["termination_status"] == PMs.LOCALLY_SOLVED
+            @test result["termination_status"] == LOCALLY_SOLVED
             @test isapprox(result["objective"], 44880; atol = 1e0)
             # @test isapprox(result["solution"]["bus"]["3"]["vm"], 0.911466*[1,1,1]; atol = 1e-3)
             vm = calc_vm_w(result, "3")
@@ -16,42 +17,38 @@
         end
 
         @testset "3-bus balanced lpubfdiag opf_bf" begin
-            pmd = PMD.parse_file("../test/data/opendss/case3_balanced.dss"; data_model="mathematical")
-            sol = PMD.run_mc_opf_bf(pmd, LPUBFDiagPowerModel, ipopt_solver)
+            pmd = parse_file("../test/data/opendss/case3_balanced.dss")
+            sol = run_mc_opf_bf(pmd, LPUBFDiagPowerModel, ipopt_solver; make_si=false)
 
-            @test sol["termination_status"] == PMs.LOCALLY_SOLVED
-            @test isapprox(sum(sol["solution"]["gen"]["1"]["pg"] * sol["solution"]["baseMVA"]), 0.0183456; atol=2e-3)
-            @test isapprox(sum(sol["solution"]["gen"]["1"]["qg"] * sol["solution"]["baseMVA"]), 0.00923328; atol=2e-3)
+            @test sol["termination_status"] == LOCALLY_SOLVED
+            @test isapprox(sum(sol["solution"]["voltage_source"]["source"]["pg"] * sol["solution"]["settings"]["sbase"]), 0.0183456; atol=2e-3)
+            @test isapprox(sum(sol["solution"]["voltage_source"]["source"]["qg"] * sol["solution"]["settings"]["sbase"]), 0.00923328; atol=2e-3)
         end
 
         @testset "3-bus unbalanced lpubfdiag opf_bf" begin
-            pmd = PMD.parse_file("../test/data/opendss/case3_unbalanced.dss"; data_model="mathematical")
-            sol = PMD.run_mc_opf_bf(pmd, LPUBFDiagPowerModel, ipopt_solver)
+            pmd = parse_file("../test/data/opendss/case3_unbalanced.dss")
+            sol = run_mc_opf_bf(pmd, LPUBFDiagPowerModel, ipopt_solver; make_si=false)
 
-            @test sol["termination_status"] == PMs.LOCALLY_SOLVED
-            @test isapprox(sum(sol["solution"]["gen"]["1"]["pg"] * sol["solution"]["baseMVA"]), 0.0214812; atol=2e-3)
-            @test isapprox(sum(sol["solution"]["gen"]["1"]["qg"] * sol["solution"]["baseMVA"]), 0.00927263; atol=2e-3)
+            @test sol["termination_status"] == LOCALLY_SOLVED
+            @test isapprox(sum(sol["solution"]["voltage_source"]["source"]["pg"] * sol["solution"]["settings"]["sbase"]), 0.0214812; atol=2e-3)
+            @test isapprox(sum(sol["solution"]["voltage_source"]["source"]["qg"] * sol["solution"]["settings"]["sbase"]), 0.00927263; atol=2e-3)
         end
     end
 
     @testset "test linearised distflow opf_bf in diagonal matrix form" begin
         @testset "5-bus lpdiagubf opf_bf" begin
-            mp_data = PowerModels.parse_file("../test/data/matpower/case5.m")
-            PMD.make_multiconductor!(mp_data, 3)
-            result = run_mc_opf_bf(mp_data, LPUBFDiagPowerModel, ipopt_solver)
+            result = run_mc_opf_bf(case5, LPUBFDiagPowerModel, ipopt_solver)
 
-            @test result["termination_status"] == PMs.LOCALLY_SOLVED
+            @test result["termination_status"] == LOCALLY_SOLVED
             @test isapprox(result["objective"], 44880; atol = 1e0)
         end
     end
 
     @testset "test linearised distflow opf_bf in full matrix form" begin
         @testset "5-bus lpfullubf opf_bf" begin
-            mp_data = PowerModels.parse_file("../test/data/matpower/case5.m")
-            PMD.make_multiconductor!(mp_data, 3)
-            result = run_mc_opf_bf(mp_data, LPUBFDiagPowerModel, ipopt_solver)
+            result = run_mc_opf_bf(case5, LPUBFDiagPowerModel, ipopt_solver)
 
-            @test result["termination_status"] == PMs.LOCALLY_SOLVED
+            @test result["termination_status"] == LOCALLY_SOLVED
             @test isapprox(result["objective"], 44880; atol = 1e0)
         end
     end
