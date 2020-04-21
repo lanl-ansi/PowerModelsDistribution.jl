1e2e638142946f788d7f6284d1cbe075ab05e496
David M Fobes <dfobes@lanl.gov>
2020-04-21 15:12:54 -0600

    REF: storage tests to eng data model

--- test/debug.jl
+++ test/debug.jl
@@ -2,10 +2,10 @@
 
 @testset "test pbs" begin
     @testset "case 3 unbalanced - ac pf pbs" begin
-        mp_data = PMD.parse_file("../test/data/opendss/case3_unbalanced.dss")
-        result = PMD.run_mc_pf_pbs(mp_data, PMs.ACPPowerModel, ipopt_solver)
+        mp_data = parse_file("../test/data/opendss/case3_unbalanced.dss")
+        result = run_mc_pf_pbs(mp_data, ACPPowerModel, ipopt_solver)
 
-        @test result["termination_status"] == PMs.LOCALLY_SOLVED
+        @test result["termination_status"] == LOCALLY_SOLVED
         @test isapprox(result["objective"], 0.0; atol=1e-4)
     end
 end
--- test/storage.jl
+++ test/storage.jl
@@ -1,56 +1,45 @@
 @info "running storage optimal power flow tests"
 
 @testset "test storage opf" begin
+    mp_data = PM.parse_file("../test/data/matpower/case5_strg.m")
+    make_multiconductor!(mp_data, 3)
+
     @testset "5-bus storage acp opf_strg" begin
-        mp_data = PowerModels.parse_file("../test/data/matpower/case5_strg.m")
-        PMD.make_multiconductor!(mp_data, 3)
+        result = run_mc_opf(mp_data, ACPPowerModel, ipopt_solver; make_si=false)
 
-        result = PMD.run_mc_opf(mp_data, PowerModels.ACPPowerModel, ipopt_solver)
-
-        @test result["termination_status"] == PMs.LOCALLY_SOLVED
+        @test result["termination_status"] == LOCALLY_SOLVED
         @test isapprox(result["objective"], 52299.2; atol = 1e0)
 
-        @test isapprox(result["solution"]["storage"]["1"]["se"],  0.0; atol = 1e0)
-        @test isapprox(result["solution"]["storage"]["2"]["se"],  0.0; atol = 1e0)
-        for c in 1:mp_data["conductors"]
-            @test isapprox(result["solution"]["storage"]["1"]["ps"][c], -0.0596928; atol = 1e-3)
-            @test isapprox(result["solution"]["storage"]["2"]["ps"][c], -0.0794600; atol = 1e-3)
-        end
+        @test isapprox(result["solution"]["storage"]["1"]["se"],  0.0; atol=1e0)
+        @test isapprox(result["solution"]["storage"]["2"]["se"],  0.0; atol=1e0)
+
+        @test all(isapprox.(result["solution"]["storage"]["1"]["ps"], -0.0596928; atol=1e-3))
+        @test all(isapprox.(result["solution"]["storage"]["2"]["ps"], -0.0794600; atol=1e-3))
     end
 
     @testset "5-bus storage dcp opf_strg" begin
-        mp_data = PowerModels.parse_file("../test/data/matpower/case5_strg.m")
-        PMD.make_multiconductor!(mp_data, 3)
+        result = run_mc_opf(mp_data, DCPPowerModel, ipopt_solver; make_si=false)
 
-        result = PMD.run_mc_opf(mp_data, PowerModels.DCPPowerModel, ipopt_solver)
-
-        @test result["termination_status"] == PMs.LOCALLY_SOLVED
+        @test result["termination_status"] == LOCALLY_SOLVED
         @test isapprox(result["objective"], 52059.6; atol = 1e0)
 
         @test isapprox(result["solution"]["storage"]["1"]["se"],  0.0; atol = 1e0)
         @test isapprox(result["solution"]["storage"]["2"]["se"],  0.0; atol = 1e0)
 
-        for c in 1:mp_data["conductors"]
-            @test isapprox(result["solution"]["storage"]["1"]["ps"][c], -0.0596443; atol = 1e-3)
-            @test isapprox(result["solution"]["storage"]["2"]["ps"][c], -0.0793700; atol = 1e-3)
-        end
+        @test all(isapprox.(result["solution"]["storage"]["1"]["ps"], -0.0596443; atol=1e-3))
+        @test all(isapprox.(result["solution"]["storage"]["2"]["ps"], -0.0793700; atol=1e-3))
     end
 
     @testset "5-bus storage nfa opf_strg" begin
-        mp_data = PowerModels.parse_file("../test/data/matpower/case5_strg.m")
-        PMD.make_multiconductor!(mp_data, 3)
+        result = run_mc_opf(mp_data, NFAPowerModel, ipopt_solver; make_si=false)
 
-        result = PMD.run_mc_opf(mp_data, PowerModels.NFAPowerModel, ipopt_solver)
-
-        @test result["termination_status"] == PMs.LOCALLY_SOLVED
+        @test result["termination_status"] == LOCALLY_SOLVED
         @test isapprox(result["objective"], 43169.9; atol = 1e0)
 
         @test isapprox(result["solution"]["storage"]["1"]["se"],  0.0; atol = 1e0)
         @test isapprox(result["solution"]["storage"]["2"]["se"],  0.0; atol = 1e0)
 
-        for c in 1:mp_data["conductors"]
-            @test isapprox(result["solution"]["storage"]["1"]["ps"][c], -0.0596443; atol = 1e-3)
-            @test isapprox(result["solution"]["storage"]["2"]["ps"][c], -0.0793700; atol = 1e-3)
-        end
+        @test all(isapprox.(result["solution"]["storage"]["1"]["ps"], -0.0596443; atol=1e-3))
+        @test all(isapprox.(result["solution"]["storage"]["2"]["ps"], -0.0793700; atol=1e-3))
     end
 end
