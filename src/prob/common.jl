1601e412e817ba9bd965fe7248d64ada6d05ade4
David M Fobes <dfobes@lanl.gov>
2020-04-21 13:57:57 -0600

    ADD: make_si kwarg for run_mc_model
    adds a way to prevent solution from being converted into SI units

--- src/prob/common.jl
+++ src/prob/common.jl
@@ -1,11 +1,11 @@
 "alias to run_model in PMs with multiconductor=true, and transformer ref extensions added by default"
-function run_mc_model(data::Dict{String,<:Any}, model_type, solver, build_mc; ref_extensions=[], kwargs...)
+function run_mc_model(data::Dict{String,<:Any}, model_type, solver, build_mc; ref_extensions=[], make_si=!get(data, "per_unit", false), kwargs...)
     if get(data, "data_model", "mathematical") == "engineering"
         data_math = transform_data_model(data, make_pu=true)
 
         result = _PM.run_model(data_math, model_type, solver, build_mc; ref_extensions=[ref_add_arcs_trans!, ref_extensions...], multiconductor=true, kwargs...)
 
-        result["solution"] = solution_math2eng(result["solution"], data_math; make_si=!get(data, "per_unit", false), make_deg=!get(data, "per_unit", false))
+        result["solution"] = solution_math2eng(result["solution"], data_math; make_si=make_si, make_deg=!get(data, "per_unit", false))
     else
         result = _PM.run_model(data, model_type, solver, build_mc; ref_extensions=[ref_add_arcs_trans!, ref_extensions...], multiconductor=true, kwargs...)
     end
