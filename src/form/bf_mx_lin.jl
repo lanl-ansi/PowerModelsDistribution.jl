478ba636fc2fbe525d159bb2a153597b7614ef83
smitha <smitha@lanl.gov>
2020-05-19 08:08:09 -0600

    feasible 240 bus

--- src/form/bf_mx_lin.jl
+++ src/form/bf_mx_lin.jl
@@ -36,8 +36,24 @@ function constraint_mc_power_losses(pm::LPUBFDiagModel, n::Int, i, f_bus, t_bus,
     w_fr = var(pm, n, :w)[f_bus]
     w_to = var(pm, n, :w)[t_bus]
 
-    JuMP.@constraint(pm.model, p_fr + p_to .== diag( g_sh_fr).*w_fr + diag( g_sh_to).*w_to)
-    JuMP.@constraint(pm.model, q_fr + q_to .== diag(-b_sh_fr).*w_fr + diag(-b_sh_to).*w_to)
+
+    fb = ref(pm, n, :bus, f_bus)
+    tb = ref(pm, n, :bus, t_bus)
+    vmax1=fb["vmax"]
+    vmax2=tb["vmax"]
+
+    cnds = conductor_ids(pm; nw=n)
+    ncnds = length(cnds)
+	
+     for c in 1:ncnds
+     if((vmax1[c]!=0) && (vmax2[c]!=0))
+       JuMP.@constraint(pm.model, (p_fr[c] + p_to[c])*1000.0 == (g_sh_fr[c,c]*w_fr[c] +  g_sh_to[c,c]*w_to[c])*1000.0)
+       JuMP.@constraint(pm.model, (q_fr[c] + q_to[c])*1000.0 == (-b_sh_fr[c,c]*w_fr[c] + -b_sh_to[c,c]*w_to[c])*1000.0)
+    else
+      b=JuMP.@constraint(pm.model, (p_fr[c] + p_to[c])*1000.0 == 0)
+       JuMP.@constraint(pm.model, (q_fr[c] + q_to[c])*1000.0 == 0)
+     end
+     end
 end
 
 
@@ -49,8 +65,17 @@ function constraint_mc_model_voltage_magnitude_difference(pm::LPUBFDiagModel, n:
     p_fr = var(pm, n, :p)[f_idx]
     q_fr = var(pm, n, :q)[f_idx]
 
-    p_s_fr = p_fr - diag(g_sh_fr).*w_fr
-    q_s_fr = q_fr + diag(b_sh_fr).*w_to
+    p_s_fr = p_fr- diag(g_sh_fr).*w_fr
+    q_s_fr = q_fr+ diag(b_sh_fr).*w_fr
+
+    fb = ref(pm, n, :bus, f_bus)
+    tb = ref(pm, n, :bus, t_bus)
+    
+    vmax1=fb["vmax"]
+    vmax2=tb["vmax"]
+    
+    cnds = conductor_ids(pm; nw=n)
+    ncnds = length(cnds)
 
     alpha = exp(-im*2*pi/3)
     Gamma = [1 alpha^2 alpha; alpha 1 alpha^2; alpha^2 alpha 1]
@@ -58,26 +83,35 @@ function constraint_mc_model_voltage_magnitude_difference(pm::LPUBFDiagModel, n:
     MP = 2*(real(Gamma).*r + imag(Gamma).*x)
     MQ = 2*(real(Gamma).*x - imag(Gamma).*r)
 
-    JuMP.@constraint(pm.model, w_to .== w_fr - MP*p_s_fr - MQ*q_s_fr)
+    for c in 1:ncnds
+    if((vmax1[c]!=0) && (vmax2[c]!=0))
+      a=JuMP.@constraint(pm.model, w_to[c]*1000.0 == 1000.0*(w_fr[c] - (MP[c,1]*p_s_fr[1]+MP[c,2]*p_s_fr[2]+MP[c,3]*p_s_fr[3]) - (MQ[c,1]*q_s_fr[1]+MQ[c,2]*q_s_fr[2]+MQ[c,3]*q_s_fr[3])))
+    else
+     #JuMP.@constraint(pm.model, p_s_fr[c]*1000.0==0)
+     #JuMP.@constraint(pm.model, q_s_fr[c]*1000.0==0)
+    end
+   end
 
 end
+
 
 
 "balanced three-phase phasor"
 function constraint_mc_theta_ref(pm::LPUBFDiagModel, n::Int, i::Int, va_ref)
     ncnds = length(conductor_ids(pm))
     @assert(ncnds >= 2)
-
     w = var(pm, n, :w)[i]
-    JuMP.@constraint(pm.model, w[2:ncnds]   .== w[1])
+    #JuMP.@constraint(pm.model, w[2:ncnds]   .== w[1])
 end
 
 
 ""
 function constraint_mc_load_power_balance(pm::LPUBFDiagModel, nw::Int, i, bus_arcs, bus_arcs_sw, bus_arcs_trans, bus_gens, bus_storage, bus_loads, bus_gs, bus_bs)
     w = var(pm, nw, :w, i)
+ bus = ref(pm, nw, :bus, i) 
+  vmax=bus["vmax"]
 
-    p = get(var(pm, nw), :p, Dict()); _PM._check_var_keys(p, bus_arcs, "active power", "branch")
+	p = get(var(pm, nw), :p, Dict()); _PM._check_var_keys(p, bus_arcs, "active power", "branch")
     q = get(var(pm, nw), :q, Dict()); _PM._check_var_keys(q, bus_arcs, "reactive power", "branch")
 
     psw  = get(var(pm, nw),  :psw, Dict()); _PM._check_var_keys(psw, bus_arcs_sw, "active power", "switch")
@@ -92,36 +126,45 @@ function constraint_mc_load_power_balance(pm::LPUBFDiagModel, nw::Int, i, bus_ar
     ps   = get(var(pm, nw),   :ps, Dict()); _PM._check_var_keys(ps, bus_storage, "active power", "storage")
     qs   = get(var(pm, nw),   :qs, Dict()); _PM._check_var_keys(qs, bus_storage, "reactive power", "storage")
 
+
     cstr_p = []
     cstr_q = []
 
-    cstr_p = JuMP.@constraint(pm.model,
-        sum(p[a] for a in bus_arcs)
-        + sum(psw[a_sw] for a_sw in bus_arcs_sw)
-        + sum(pt[a_trans] for a_trans in bus_arcs_trans)
-        .==
-        sum(pg[g] for g in bus_gens)
-        - sum(ps[s] for s in bus_storage)
-        - sum(pd[d] for d in bus_loads)
-        - sum(gs.*w for gs in values(bus_gs))
-    )
+   cnds = conductor_ids(pm; nw=nw)
+   ncnds = length(cnds)
 
-    cstr_q = JuMP.@constraint(pm.model,
-        sum(q[a] for a in bus_arcs)
-        + sum(qsw[a_sw] for a_sw in bus_arcs_sw)
-        + sum(qt[a_trans] for a_trans in bus_arcs_trans)
-        .==
-        sum(qg[g] for g in bus_gens)
-        - sum(qs[s] for s in bus_storage)
-        - sum(qd[d] for d in bus_loads)
-        + sum(bs.*w for bs in values(bus_bs))
-    )
+   for c in 1:ncnds
+     if(vmax[c]!=0)
+       cstr_p = JuMP.@constraint(pm.model,
+        sum((p[a])[c]*1000.0 for a in bus_arcs)
+        + sum((psw[a_sw])[c]*1000.0 for a_sw in bus_arcs_sw)
+        + sum((pt[a_trans])[c]*1000.0 for a_trans in bus_arcs_trans)
+        == 
+        sum((pg[g])[c]*1000.0 for g in bus_gens)
+        - sum((ps[s])[c]*1000.0 for s in bus_storage)
+        - sum((pd[d])[c]*1000.0 for d in bus_loads) 
+	- sum(gs[c]*w[c] for gs in values(bus_gs)))
+    end
+    end
 
-    con(pm, nw, :lam_kcl_r)[i] = cstr_p
-    con(pm, nw, :lam_kcl_i)[i] = cstr_q
+
+    for c in 1:ncnds
+      if(vmax[c]!=0)
+       cstr_q = JuMP.@constraint(pm.model,
+        sum((q[a])[c]*1000.0 for a in bus_arcs)
+        + sum((qsw[a_sw])[c]*1000.0 for a_sw in bus_arcs_sw)
+        + sum((qt[a_trans])[c]*1000.0 for a_trans in bus_arcs_trans)
+        ==
+        sum((qg[g])[c]*1000.0 for g in bus_gens)
+        - sum((qs[s])[c]*1000.0 for s in bus_storage)
+        - sum((qd[d])[c]*1000.0 for d in bus_loads)
+        + sum(bs[c]*w[c] for bs in values(bus_bs)))
+   end
+   end
 
     if _IM.report_duals(pm)
         sol(pm, nw, :bus, i)[:lam_kcl_r] = cstr_p
         sol(pm, nw, :bus, i)[:lam_kcl_i] = cstr_q
     end
 end
+
