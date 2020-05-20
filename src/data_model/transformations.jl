e7f2fd2f62319c853cfdf922b246d359e86555fd
David M Fobes <dfobes@lanl.gov>
2020-05-20 14:44:21 -0600

    FIX: padding issues

--- src/data_model/transformations.jl
+++ src/data_model/transformations.jl
@@ -68,11 +68,11 @@ end
 "kron reduction"
 function apply_kron_reduction!(data_eng::Dict{String,<:Any}; kr_phases::Union{Vector{Int},Vector{String}}=[1,2,3], kr_neutral::Union{Int,String}=4)
     if haskey(data_eng, "bus")
-        for (_, eng_obj) in data_eng["bus"]
+        for (id,eng_obj) in data_eng["bus"]
             filter = eng_obj["terminals"] .!= kr_neutral
             terminals_kr = eng_obj["terminals"][filter]
 
-            @assert all(t in kr_phases for t in terminals_kr) "bus $name has terminals $(eng_obj["terminals"]), outside of $kr_phases, cannot be kron reduced"
+            @assert all(t in kr_phases for t in terminals_kr) "bus $id has terminals $(eng_obj["terminals"]), outside of $kr_phases, cannot be kron reduced"
 
             _apply_filter!(eng_obj, ["vm", "va", "vm_lb", "vm_ub"], filter)
             eng_obj["terminals"] = terminals_kr
@@ -83,7 +83,7 @@ function apply_kron_reduction!(data_eng::Dict{String,<:Any}; kr_phases::Union{Ve
     end
 
     if haskey(data_eng, "line")
-        for (_, eng_obj) in data_eng["line"]
+        for (_,eng_obj) in data_eng["line"]
             @assert all(eng_obj["f_connections"].==eng_obj["t_connections"]) "Kron reduction is only supported if f_connections == t_connections"
 
             _apply_linecode!(eng_obj, data_eng)
@@ -116,7 +116,7 @@ function apply_kron_reduction!(data_eng::Dict{String,<:Any}; kr_phases::Union{Ve
     end
 
     if haskey(data_eng, "switch")
-        for (_, eng_obj) in data_eng["switch"]
+        for (_,eng_obj) in data_eng["switch"]
             @assert all(eng_obj["f_connections"].==eng_obj["t_connections"]) "Kron reduction is only supported if f_connections == t_connections"
 
             _apply_linecode!(eng_obj, data_eng)
@@ -127,14 +127,14 @@ function apply_kron_reduction!(data_eng::Dict{String,<:Any}; kr_phases::Union{Ve
     end
 
     if haskey(data_eng, "shunt")
-        for (_, eng_obj) in data_eng["shunt"]
+        for (_,eng_obj) in data_eng["shunt"]
             filter = _kron_reduce_branch!(eng_obj, Vector{String}([]), ["gs", "bs"], eng_obj["connections"], kr_neutral)
             _apply_filter!(eng_obj, ["connections"], filter)
         end
     end
 
     if haskey(data_eng, "load")
-        for (_, eng_obj) in data_eng["load"]
+        for (_,eng_obj) in data_eng["load"]
             if eng_obj["configuration"]==WYE
                 @assert eng_obj["connections"][end] == kr_neutral "for wye-connected loads, to kron reduce the connections list should end with a neutral"
 
@@ -145,7 +145,7 @@ function apply_kron_reduction!(data_eng::Dict{String,<:Any}; kr_phases::Union{Ve
     end
 
     if haskey(data_eng, "generator")
-        for (_, eng_obj) in data_eng["generator"]
+        for (_,eng_obj) in data_eng["generator"]
             if eng_obj["configuration"]==WYE
                 @assert eng_obj["connections"][end] == kr_neutral "for wye-connected generators, to kron reduce the connections list should end with a neutral"
 
@@ -156,7 +156,7 @@ function apply_kron_reduction!(data_eng::Dict{String,<:Any}; kr_phases::Union{Ve
     end
 
     if haskey(data_eng, "solar")
-        for (_, eng_obj) in data_eng["solar"]
+        for (_,eng_obj) in data_eng["solar"]
             if eng_obj["configuration"]==WYE
                 @assert eng_obj["connections"][end] == kr_neutral "for wye-connected solar, to kron reduce the connections list should end with a neutral"
 
@@ -167,14 +167,14 @@ function apply_kron_reduction!(data_eng::Dict{String,<:Any}; kr_phases::Union{Ve
     end
 
     if haskey(data_eng, "storage")
-        for (_, eng_obj) in data_eng["storage"]
+        for (_,eng_obj) in data_eng["storage"]
             filter = eng_obj["connections"] .!= kr_neutral
             _apply_filter!(eng_obj, ["connections"], filter)
         end
     end
 
     if haskey(data_eng, "voltage_source")
-        for (_, eng_obj) in data_eng["voltage_source"]
+        for (_,eng_obj) in data_eng["voltage_source"]
             filter = eng_obj["connections"] .!= kr_neutral
             _apply_filter!(eng_obj, ["connections"], filter)
         end
@@ -182,6 +182,8 @@ function apply_kron_reduction!(data_eng::Dict{String,<:Any}; kr_phases::Union{Ve
 
     delete!(data_eng, "linecode")  # kron reduction moves linecode properties directly to lines
     delete!(data_eng, "xfmrcode")  # kron reduction moves xfmrcode properties directly to transformers
+
+    data_eng["is_kron_reduced"] = true
 end
 
 
@@ -190,7 +192,7 @@ function pad_mc_properties!(data_eng::Dict{String,<:Any})
     all_conductors = _get_complete_conductor_set(data_eng)
 
     if haskey(data_eng, "bus")
-        for (_, eng_obj) in data_eng["bus"]
+        for (_,eng_obj) in data_eng["bus"]
             if !haskey(eng_obj, "vm_lb")
                 eng_obj["vm_lb"] = fill(0.0, length(eng_obj["terminals"]))
             end
@@ -201,12 +203,12 @@ function pad_mc_properties!(data_eng::Dict{String,<:Any})
 
             _pad_properties!(eng_obj, ["vm", "va", "vm_lb", "vm_ub"], eng_obj["terminals"], all_conductors)
 
-            eng_obj["terminals"] = all_conductors
+            _pad_connections!(eng_obj, "terminals", all_conductors)
         end
     end
 
     if haskey(data_eng, "line")
-        for (_, eng_obj) in data_eng["line"]
+        for (_,eng_obj) in data_eng["line"]
             _apply_linecode!(eng_obj, data_eng)
 
             _pad_properties!(eng_obj, ["rs", "xs", "g_fr", "g_to", "b_fr", "b_to"], eng_obj["f_connections"], all_conductors)
@@ -218,32 +220,34 @@ function pad_mc_properties!(data_eng::Dict{String,<:Any})
                 end
             end
 
-            eng_obj["f_connections"] = eng_obj["t_connections"] = all_conductors
+            _pad_connections!(eng_obj, "f_connections", all_conductors)
+            _pad_connections!(eng_obj, "t_connections", all_conductors)
         end
     end
 
     if haskey(data_eng, "transformer")
-        for (id, eng_obj) in data_eng["transformer"]
+        for (_,eng_obj) in data_eng["transformer"]
             _apply_xfmrcode!(eng_obj, data_eng)
 
             if haskey(eng_obj, "f_connections")
                 _pad_properties!(eng_obj, ["tm_lb", "tm_ub", "tm_set"], eng_obj["f_connections"], all_conductors; pad_value=1.0)
                 _pad_properties!(eng_obj, ["tm_fix"], eng_obj["f_connections"], all_conductors; pad_value=false)
 
-                eng_obj["f_connections"] = eng_obj["t_connections"] = all_conductors
+                _pad_connections!(eng_obj, "f_connections", all_conductors)
+                _pad_connections!(eng_obj, "t_connections", all_conductors)
             else
                 for (w, connections) in enumerate(eng_obj["connections"])
                     _pad_properties!(eng_obj, ["tm_lb", "tm_ub", "tm_set"], w, connections, all_conductors; pad_value=1.0)
                     _pad_properties!(eng_obj, ["tm_fix"], w, connections, all_conductors; pad_value=false)
 
-                    eng_obj["connections"][w] = all_conductors
+                    _pad_connections!(eng_obj, "connections", w, all_conductors)
                 end
             end
         end
     end
 
     if haskey(data_eng, "switch")
-        for (_, eng_obj) in data_eng["switch"]
+        for (_,eng_obj) in data_eng["switch"]
             _apply_linecode!(eng_obj, data_eng)
 
             _pad_properties!(eng_obj, ["rs", "xs"], eng_obj["f_connections"], all_conductors)
@@ -255,66 +259,70 @@ function pad_mc_properties!(data_eng::Dict{String,<:Any})
                 end
             end
 
-            eng_obj["f_connections"] = all_conductors
-            eng_obj["t_connections"] = all_conductors
+            _pad_connections!(eng_obj, "f_connections", all_conductors)
+            _pad_connections!(eng_obj, "t_connections", all_conductors)
         end
     end
 
     if haskey(data_eng, "shunt")
-        for (_, eng_obj) in data_eng["shunt"]
+        for (_,eng_obj) in data_eng["shunt"]
             _pad_properties!(eng_obj, ["gs", "bs"], eng_obj["connections"], all_conductors)
 
-            eng_obj["connections"] = all_conductors
+            _pad_connections!(eng_obj, "connections", all_conductors)
         end
     end
 
     if haskey(data_eng, "load")
-        for (_, eng_obj) in data_eng["load"]
+        for (_,eng_obj) in data_eng["load"]
             if eng_obj["configuration"] == WYE
                 _pad_properties!(eng_obj, ["pd_nom", "qd_nom"], eng_obj["connections"], all_conductors)
             else
                 _pad_properties_delta!(eng_obj, ["pd_nom", "qd_nom"], eng_obj["connections"], all_conductors)
             end
 
-            eng_obj["connections"] = all_conductors
+            _pad_connections!(eng_obj, "connections", all_conductors)
         end
     end
 
     if haskey(data_eng, "generator")
-        for (_, eng_obj) in data_eng["generator"]
+        for (_,eng_obj) in data_eng["generator"]
             if eng_obj["configuration"]==WYE
                 _pad_properties!(eng_obj, ["pg", "qg", "vg", "pg_lb", "pg_ub", "qg_lb", "qg_ub"], eng_obj["connections"], all_conductors)
             else
                 _pad_properties_delta!(eng_obj, ["pg", "qg", "vg", "pg_lb", "pg_ub", "qg_lb", "qg_ub"], eng_obj["connections"], all_conductors)
             end
+
+            _pad_connections!(eng_obj, "connections", all_conductors)
         end
     end
 
     if haskey(data_eng, "solar")
-        for (_, eng_obj) in data_eng["solar"]
+        for (_,eng_obj) in data_eng["solar"]
             if eng_obj["configuration"]==WYE
                 _pad_properties!(eng_obj, ["pg", "qg", "vg", "pg_lb", "pg_ub", "qg_lb", "qg_ub"], eng_obj["connections"], all_conductors)
             else
                 _pad_properties_delta!(eng_obj, ["pg", "qg", "vg", "pg_lb", "pg_ub", "qg_lb", "qg_ub"], eng_obj["connections"], all_conductors)
             end
 
-            eng_obj["connections"] = all_conductors
+            _pad_connections!(eng_obj, "connections", all_conductors)
         end
     end
 
     if haskey(data_eng, "storage")
-        for (_, eng_obj) in data_eng["storage"]
+        for (_,eng_obj) in data_eng["storage"]
             _pad_properties!(eng_obj, ["cm_ub", "qs_lb", "qs_ub", "rs", "xs", "ps", "qs"], eng_obj["connections"], all_conductors)
 
-            eng_obj["connections"] = all_conductors
+            _pad_connections!(eng_obj, "connections", all_conductors)
         end
     end
 
     if haskey(data_eng, "voltage_source")
-        for (_, eng_obj) in data_eng["voltage_source"]
+        for (_,eng_obj) in data_eng["voltage_source"]
             _pad_properties!(eng_obj, ["pg", "qg", "pg_lb", "pg_ub", "qg_lb", "qg_ub", "vm", "va"], eng_obj["connections"], all_conductors)
 
-            eng_obj["connections"] = all_conductors
+            _pad_connections!(eng_obj, "connections", all_conductors)
         end
     end
+
+    data_eng["is_padded"] = true
 end
