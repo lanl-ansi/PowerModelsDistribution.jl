5923bc260144dd5bb46aa0d1c8449163e828d4f5
David M Fobes <dfobes@lanl.gov>
2020-06-18 11:22:48 -0600

    ADD: Storage variables and constraints to UBF OPF
    storage variables and constraints were missing from the UBF form
    version of the opf problem

--- src/prob/opf.jl
+++ src/prob/opf.jl
@@ -122,6 +122,7 @@ function build_mc_opf(pm::AbstractUBFModels)
     variable_mc_transformer_power(pm)
     variable_mc_gen_power_setpoint(pm)
     variable_mc_load_setpoint(pm)
+    variable_mc_storage_power(pm)
 
     # Constraints
     constraint_mc_model_current(pm)
@@ -142,6 +143,13 @@ function build_mc_opf(pm::AbstractUBFModels)
 
     for i in ids(pm, :bus)
         constraint_mc_load_power_balance(pm, i)
+    end
+
+    for i in ids(pm, :storage)
+        _PM.constraint_storage_state(pm, i)
+        _PM.constraint_storage_complementarity_nl(pm, i)
+        constraint_mc_storage_losses(pm, i)
+        constraint_mc_storage_thermal_limit(pm, i)
     end
 
     for i in ids(pm, :branch)
