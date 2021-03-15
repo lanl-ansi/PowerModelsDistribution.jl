2891788bdbb5864a4bc1f0e0bdd0b29ffcbebca9
David M Fobes <dfobes@lanl.gov>
2021-03-15 08:18:53 -0600

    REF: run_model to _solve_mc_model

--- src/core/base.jl
+++ src/core/base.jl
@@ -10,54 +10,6 @@ _IM.@def pmd_fields begin
 end
 
 
-""
-function run_model(data::Dict{String,<:Any}, model_type::Type, optimizer, build_method;
-        ref_extensions=[], solution_processors=[], relax_integrality=false,
-        multinetwork=false, multiconductor=false, kwargs...)
-
-    if multinetwork != _IM.ismultinetwork(data)
-        model_requirement = multinetwork ? "multi-network" : "single-network"
-        data_type = _IM.ismultinetwork(data) ? "multi-network" : "single-network"
-        Memento.error(_LOGGER, "attempted to build a $(model_requirement) model with $(data_type) data")
-    end
-
-    if multiconductor != ismulticonductor(data)
-        model_requirement = multiconductor ? "multi-conductor" : "single-conductor"
-        data_type = ismulticonductor(data) ? "multi-conductor" : "single-conductor"
-        Memento.error(_LOGGER, "attempted to build a $(model_requirement) model with $(data_type) data")
-    end
-
-    start_time = time()
-    pm = instantiate_mc_model(data, model_type, build_method; ref_extensions=ref_extensions, kwargs...)
-    Memento.debug(_LOGGER, "pm model build time: $(time() - start_time)")
-
-    start_time = time()
-    result = optimize_model!(pm, relax_integrality=relax_integrality, optimizer=optimizer, solution_processors=solution_processors)
-    Memento.debug(_LOGGER, "pm model solve and solution time: $(time() - start_time)")
-
-    return result
-end
-
-
-"multiconductor version of instantiate model from PowerModels"
-function instantiate_mc_model(data::Dict{String,<:Any}, model_type::Type, build_method::Function; ref_extensions::Vector{<:Function}=Vector{Function}([]), kwargs...)
-    if get(data, "data_model", MATHEMATICAL) == ENGINEERING
-        Memento.info(_LOGGER, "Converting ENGINEERING data model to MATHEMATICAL first to build JuMP model")
-        data = transform_data_model(data)
-    end
-
-    return _IM.instantiate_model(
-        data, model_type, build_method, ref_add_core!, _pmd_global_keys,
-        pmd_it_sym; ref_extensions = [ref_extensions..., ref_add_arcs_switch!,
-        ref_add_arcs_transformer!, ref_add_connections!], kwargs...)
-end
-
-
-""
-ismulticonductor(pm::AbstractMCPowerModel, nw::Int) = haskey(pm.ref[:it][pmd_it_sym][:nw][nw], :conductors)
-ismulticonductor(pm::AbstractMCPowerModel; nw::Int=nw_id_default) = haskey(pm.ref[:it][pmd_it_sym][:nw][nw], :conductors)
-
-
 # Helper functions for multinetwork AbstractPowerModel objects.
 ismultinetwork(pm::AbstractMCPowerModel) = ismultinetwork(pm, pmd_it_sym)
 nw_ids(pm::AbstractMCPowerModel) = _IM.nw_ids(pm, pmd_it_sym)
--- src/prob/common.jl
+++ src/prob/common.jl
@@ -1,20 +1,57 @@
-"alias to run_model in PowerModels with multiconductor=true, and transformer ref extensions added by default"
+""
+function _solve_mc_model(data::Dict{String,<:Any}, model_type::Type, optimizer, build_method;
+        ref_extensions=[], solution_processors=[], relax_integrality=false,
+        multinetwork=false, kwargs...)
+
+    if multinetwork != ismultinetwork(data)
+        model_requirement = multinetwork ? "multi-network" : "single-network"
+        data_type = ismultinetwork(data) ? "multi-network" : "single-network"
+        Memento.error(_LOGGER, "attempted to build a $(model_requirement) model with $(data_type) data")
+    end
+
+    start_time = time()
+    pm = instantiate_mc_model(data, model_type, build_method; ref_extensions=ref_extensions, kwargs...)
+    Memento.debug(_LOGGER, "pm model build time: $(time() - start_time)")
+
+    start_time = time()
+    result = optimize_model!(pm, relax_integrality=relax_integrality, optimizer=optimizer, solution_processors=solution_processors)
+    Memento.debug(_LOGGER, "pm model solve and solution time: $(time() - start_time)")
+
+    return result
+end
+
+
+""
+function instantiate_mc_model(data::Dict{String,<:Any}, model_type::Type, build_method::Function; ref_extensions::Vector{<:Function}=Vector{Function}([]), kwargs...)
+    if get(data, "data_model", MATHEMATICAL) == ENGINEERING
+        Memento.info(_LOGGER, "Converting ENGINEERING data model to MATHEMATICAL first to build JuMP model")
+        data = transform_data_model(data)
+    end
+
+    return _IM.instantiate_model(
+        data, model_type, build_method, ref_add_core!, _pmd_global_keys,
+        pmd_it_sym; ref_extensions = [ref_extensions..., ref_add_arcs_switch!,
+        ref_add_arcs_transformer!, ref_add_connections!], kwargs...)
+end
+
+
+""
 function solve_mc_model(data::Dict{String,<:Any}, model_type::Type, solver, build_mc::Function; ref_extensions::Vector{<:Function}=Vector{Function}([]), make_si=!get(data, "per_unit", false), multinetwork::Bool=false, kwargs...)::Dict{String,Any}
     if get(data, "data_model", MATHEMATICAL) == ENGINEERING
         data_math = transform_data_model(data; build_multinetwork=multinetwork)
 
-        result = run_model(data_math, model_type, solver, build_mc; ref_extensions=[ref_add_arcs_transformer!, ref_add_arcs_switch!, ref_add_connections!, ref_extensions...], multiconductor=true, multinetwork=multinetwork, kwargs...)
+        result = _solve_mc_model(data_math, model_type, solver, build_mc; ref_extensions=[ref_add_arcs_transformer!, ref_add_arcs_switch!, ref_add_connections!, ref_extensions...], multinetwork=multinetwork, kwargs...)
 
         result["solution"] = transform_solution(result["solution"], data_math; make_si=make_si)
     elseif get(data, "data_model", MATHEMATICAL) == MATHEMATICAL
-        result = run_model(data, model_type, solver, build_mc; ref_extensions=[ref_add_arcs_transformer!, ref_add_arcs_switch!, ref_add_connections!, ref_extensions...], multiconductor=true, multinetwork=multinetwork, kwargs...)
+        result = _solve_mc_model(data, model_type, solver, build_mc; ref_extensions=[ref_add_arcs_transformer!, ref_add_arcs_switch!, ref_add_connections!, ref_extensions...], multinetwork=multinetwork, kwargs...)
     end
 
     return result
 end
 
 
-"alias to run_model in PowerModels with multiconductor=true, and transformer ref extensions added by default"
+""
 function solve_mc_model(file::String, model_type::Type, solver, build_mc::Function; ref_extensions::Vector{<:Function}=Vector{Function}([]), kwargs...)::Dict{String,Any}
     return solve_mc_model(parse_file(file), model_type, solver, build_mc; ref_extensions=ref_extensions, kwargs...)
 end
