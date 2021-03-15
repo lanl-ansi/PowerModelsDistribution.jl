b7f5550122226aa0d2ed0be28131faafd8356add
David M Fobes <dfobes@lanl.gov>
2021-03-15 14:33:49 -0600

    REF: sol_polar_voltage! to core/solution.jl

--- src/core/solution.jl
+++ src/core/solution.jl
@@ -50,6 +50,7 @@ function _sol_data_model_w!(solution::Dict{String,<:Any})
 end
 
 
+ ""
 function sol_data_model!(pm::AbstractWModels, solution::Dict{String,<:Any})
     apply_pmd!(_sol_data_model_w!, solution)
 end
@@ -79,6 +80,7 @@ function _sol_data_model_acr!(solution::Dict{String,<:Any})
 end
 
 
+""
 function sol_data_model!(pm::AbstractACRModel, solution::Dict{String,<:Any})
     apply_pmd!(_sol_data_model_acr!, solution)
 end
@@ -87,4 +89,38 @@ end
 "converts the solution data into the data model's standard space, polar voltages and rectangular power"
 function sol_data_model!(pm::AbstractMCPowerModel, solution::Dict{String,<:Any})
     Memento.warn(_LOGGER, "sol_data_model! not defined for power model of type $(typeof(pm))")
+end
+
+
+""
+function sol_polar_voltage!(pm::AbstractMCPowerModel, solution::Dict{String,<:Any})
+    apply_pmd!(_sol_polar_voltage!, solution)
+end
+
+
+""
+function _sol_polar_voltage!(solution::Dict{String,<:Any})
+    if haskey(solution, "nw")
+        nws_data = solution["nw"]
+    else
+        nws_data = Dict("0" => solution)
+    end
+
+    for (n, nw_data) in nws_data
+        if haskey(nw_data, "bus")
+            for (i,bus) in nw_data["bus"]
+                if haskey(bus, "vr") && haskey(bus, "vi")
+                    vr = bus["vr"]
+                    vi = bus["vi"]
+                    if isa(vr, Dict)
+                        bus["vm"] = Dict(t=>abs(vr[t]+im*vi[t]) for t in keys(vr))
+                        bus["va"] = Dict(t=>_wrap_to_pi(atan(vi[t], vr[t])) for t in keys(vr))
+                    else
+                        bus["vm"] = abs.(vr .+ im*vi)
+                        bus["va"] = _wrap_to_pi(atan.(vi, vr))
+                    end
+                end
+            end
+        end
+    end
 end
