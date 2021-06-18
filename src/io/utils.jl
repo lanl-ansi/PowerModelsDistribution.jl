d0a364397f24eacadbea81998785e4359373ab05
sanderclaeys <id@sanderclaeys.eu>
2021-06-18 20:42:50 +0200

    [ADD][4W-EXTENSION] Parsing, transformations and solution builder (#339)
    * bug in _correct_branch_directions!
    
    * solution builder
    
    when the data type is Any for a DenseAxisArray, call the solution builder for each element to correctly specialize for each type in the vector
    
    * optional phase projection
    
    added a keyword to turn of phase projection
    
    * math transformations
    
    Added a keyword to pass transformations at the mathematical model level (analogous to transformations in the parser). This is useful for the voltage starting value methods.
    
    * reactor series impedance
    
    Pass through the series impedance values. This allows for correct handling of groundings implemented as reactors.
    
    * bug edge case
    
    Prevents errors when a line connects only to a zero terminal. This happens for groundings.
    
    * voltage source parsing
    
    Prevents copying of sourcebus terminals which are not part of the voltage source (occurs because a short-circuited terminal is sometimes linked to the neutral to prevent OpenDSS from omitting it from the results).
    
    * line reduction transformations
    
    Adds transformations which reduce the lines in an engineering data model.
    
    * no-load initialization
    
    Calculates and adds no-load voltage starting values to the mathematical data model. This is key to speed up and achieve convergence in complex cases.
    
    * line loops
    
    Resolves line loops (lines connected to a single bus, typically result of grounding reactors which are parsed to lines).
    
    * Kron-reduction
    
    Applies a Kron-reduction at the data engineering model level, by detecting the implicit neutrals in the network.
    
    * typo
    
    * vstart refactor bug
    
    * typo
    
    * voltage bound transformations
    
    * combine vbounds
    
    data model transformation combines various  ways of defining voltage bounds to the tightest unique set covering all bounds
    
    * bug
    
    * pp_pn_ng vbounds
    
    * bug fixes
    
    * tap prop removed
    
    * phase_projected docstring
    
    * transformations removed
    
    * connections edge cases
    
    * tripple double quotes
    
    * verbose removed
    
    * added docstrings

--- src/io/utils.jl
+++ src/io/utils.jl
@@ -385,16 +385,16 @@ function _discover_terminals!(data_eng::Dict{String,<:Any})
     if haskey(data_eng, "line")
         for (_,eng_obj) in data_eng["line"]
             # ignore 0 terminal
-            push!(terminals[eng_obj["f_bus"]], setdiff(eng_obj["f_connections"], [0])...)
-            push!(terminals[eng_obj["t_bus"]], setdiff(eng_obj["t_connections"], [0])...)
+            !all(eng_obj["f_connections"] .== 0) && push!(terminals[eng_obj["f_bus"]], setdiff(eng_obj["f_connections"], [0])...)
+            !all(eng_obj["t_connections"] .== 0) && push!(terminals[eng_obj["t_bus"]], setdiff(eng_obj["t_connections"], [0])...)
         end
     end
 
     if haskey(data_eng, "switch")
         for (_,eng_obj) in data_eng["switch"]
             # ignore 0 terminal
-            push!(terminals[eng_obj["f_bus"]], setdiff(eng_obj["f_connections"], [0])...)
-            push!(terminals[eng_obj["t_bus"]], setdiff(eng_obj["t_connections"], [0])...)
+            !all(eng_obj["f_connections"] .== 0) && push!(terminals[eng_obj["f_bus"]], setdiff(eng_obj["f_connections"], [0])...)
+            !all(eng_obj["t_connections"] .== 0) && push!(terminals[eng_obj["t_bus"]], setdiff(eng_obj["t_connections"], [0])...)
         end
     end
 
@@ -402,14 +402,14 @@ function _discover_terminals!(data_eng::Dict{String,<:Any})
         for (_,tr) in data_eng["transformer"]
             for w in 1:length(tr["bus"])
                 # ignore 0 terminal
-                push!(terminals[tr["bus"][w]], setdiff(tr["connections"][w], [0])...)
+                tr["connections"][w]!=[0] && push!(terminals[tr["bus"][w]], setdiff(tr["connections"][w], [0])...)
             end
         end
     end
 
     for comp_type in [x for x in ["voltage_source", "load", "generator", "solar"] if haskey(data_eng, x)]
         for comp in values(data_eng[comp_type])
-            push!(terminals[comp["bus"]], setdiff(comp["connections"], [0])...)
+            !all(comp["connections"] .== 0) && push!(terminals[comp["bus"]], setdiff(comp["connections"], [0])...)
         end
     end
 
