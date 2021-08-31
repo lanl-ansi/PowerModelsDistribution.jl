a2c3a2321f988b728be1032331f022dd482ab61a
keatsig <42916010+keatsig@users.noreply.github.com>
2021-08-31 15:52:08 -0600

    FIX: linearized lower voltage magnitude limits for FBS formulation (#353)
    * Added FBS linear UBF formulation
    
    * Added documentation to FBS model
    
    * Updated documentation to FBS model
    
    * Added RegControl
    
    * Added test case for RegControl
    
    * Update test case for RegControl
    
    * Added documentation for RegControl
    
    * Revert "Added documentation for RegControl"
    
    This reverts commit fb6f6b610c37952bcf7ffa6272d6a8617ab1f734.
    
    * Updated documentation for RegControl
    
    * Update dss2eng.jl
    
    * Updated unit tests and documentation for RegControl
    
    * Updated unit tests and documentation for RegControl
    
    * Updated RegControl objects in data model
    
    * Updated RegControl objects in data model
    
    * Added test case for RegControl parser check
    
    * Fixed lower voltage limits for FBS linear formulation
    
    * Revert "Fixed lower voltage limits for FBS linear formulation"
    
    This reverts commit 08a036c4580239a98405f562f6e0cd671d884847.
    
    * Fixed lower voltage magnitude limits for FBS linear formulation
    
    * Update bf_fbs.jl
    
    * Update opf_bf.jl
    
    * Updated lower voltage limits for FBS linear formulation
    
    * Updated lower voltage limits for FBS linear formulation

--- src/form/bf_fbs.jl
+++ src/form/bf_fbs.jl
@@ -103,17 +103,22 @@ function constraint_mc_voltage_magnitude_bounds(pm::FBSUBFPowerModel, nw::Int, i
     vr0 = var(pm, nw, :vr0, i)
     vi0 = var(pm, nw, :vi0, i)
 
-    for (idx,t) in enumerate(ref(pm, nw, :bus, i)["terminals"]) 
-        JuMP.@constraint(pm.model, vr[t]^2 + vi[t]^2 >= vmin[idx]^2)
-        # TODO: first-order approximation might work with OpenDSS power-flow as initial operating point 
-        # JuMP.@constraint(pm.model, 2*vr[t]*vr0[t] + 2*vi[t]*vi0[t] - vr0[t]^2 - vi0[t]^2 >= vmin[idx]^2)      
-
+    for (idx,t) in enumerate(ref(pm, nw, :bus, i)["terminals"])
+        # linearized lower voltage magnitude limits at reference bus
+        if ref(pm, nw, :bus, i)["bus_type"] == 3.0 && vmax[idx] == vmin[idx]
+            vr_ref = ref(pm, nw, :bus, i)["vm"][idx]*cos(ref(pm, nw, :bus, i)["va"][idx])
+            vi_ref = ref(pm, nw, :bus, i)["vm"][idx]*sin(ref(pm, nw, :bus, i)["va"][idx])
+            JuMP.@constraint(pm.model, 2*vr[t]*vr_ref + 2*vi[t]*vi_ref - vr_ref^2 - vi_ref^2 >= vmin[idx]^2)
+        # linearized lower voltage magnitude limits at at all other buses
+        else
+            JuMP.@constraint(pm.model, 2*vr[t]*vr0[t] + 2*vi[t]*vi0[t] - vr0[t]^2 - vi0[t]^2 >= vmin[idx]^2)
+        end
         # Outer approximation of upper voltage magnitude limits
         if vmax[idx] < Inf  
             JuMP.@constraint(pm.model, -vmax[idx] <= vr[t])
-            JuMP.@constraint(pm.model,  vmax[idx] >= vr[t])    
+            JuMP.@constraint(pm.model,  vmax[idx] >= vr[t])
             JuMP.@constraint(pm.model, -vmax[idx] <= vi[t])
-            JuMP.@constraint(pm.model,  vmax[idx] >= vi[t]) 
+            JuMP.@constraint(pm.model,  vmax[idx] >= vi[t])
             JuMP.@constraint(pm.model, -sqrt(2)*vmax[idx] <= vr[t] + vi[t])
             JuMP.@constraint(pm.model,  sqrt(2)*vmax[idx] >= vr[t] + vi[t])
             JuMP.@constraint(pm.model, -sqrt(2)*vmax[idx] <= vr[t] - vi[t])
