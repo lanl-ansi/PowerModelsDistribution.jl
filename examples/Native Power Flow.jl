027e30a620d6b1fac92161dfa3502f8d1a81d9af
Rahmat Heidari <heidari.rahmat@gmail.com>
2022-08-19 21:03:56 +1000

    refactoring

--- examples/Native Power Flow.jl
+++ examples/Native Power Flow.jl
@@ -1,277 +1,108 @@
 using Pkg
 using PowerModelsDistribution
+using JSON
 
-Pkg.activate("./examples")
+# Pkg.activate("./examples")
 
-using PowerModelsDistribution
 const PMD = PowerModelsDistribution
 
+include("Native Power Flow - Data Cleaning Functions.jl")
+case_path = "examples/native_pf_testcases"
+solution_dir = "examples/native_pf_testcases/solutions"
+
 ##
-function vsource_correction_to_4w!(data_eng)
-    if haskey(data_eng, "multinetwork")
-        for (n,nw) in data_eng["nw"]
-            nw["voltage_source"]["source"]["rs"][4,4] = nw["voltage_source"]["source"]["rs"][1,1]
-            nw["voltage_source"]["source"]["rs"][1:3,4] .= nw["voltage_source"]["source"]["rs"][1,2]
-            nw["voltage_source"]["source"]["rs"][4,1:3] .= nw["voltage_source"]["source"]["rs"][1,2]
-            nw["voltage_source"]["source"]["xs"][4,4] = nw["voltage_source"]["source"]["xs"][1,1]
-            nw["voltage_source"]["source"]["xs"][1:3,4] .= nw["voltage_source"]["source"]["xs"][1,2]
-            nw["voltage_source"]["source"]["xs"][4,1:3] .= nw["voltage_source"]["source"]["xs"][1,2]
-        end
-    else
-        data_eng["voltage_source"]["source"]["rs"][4,4] = data_eng["voltage_source"]["source"]["rs"][1,1]
-        data_eng["voltage_source"]["source"]["rs"][1:3,4] .= data_eng["voltage_source"]["source"]["rs"][1,2]
-        data_eng["voltage_source"]["source"]["rs"][4,1:3] .= data_eng["voltage_source"]["source"]["rs"][1,2]
-        data_eng["voltage_source"]["source"]["xs"][4,4] = data_eng["voltage_source"]["source"]["xs"][1,1]
-        data_eng["voltage_source"]["source"]["xs"][1:3,4] .= data_eng["voltage_source"]["source"]["xs"][1,2]
-        data_eng["voltage_source"]["source"]["xs"][4,1:3] .= data_eng["voltage_source"]["source"]["xs"][1,2]        
-    end
-    return nothing
-end
 
-function vsource_correction_to_3w!(data_eng)
-    if haskey(data_eng, "multinetwork")
-        for (n,nw) in data_eng["nw"]
-            nw["voltage_source"]["source"]["rs"] = nw["voltage_source"]["source"]["rs"][1:3,1:3]
-            nw["voltage_source"]["source"]["xs"] = nw["voltage_source"]["source"]["xs"][1:3,1:3]
-            nw["voltage_source"]["source"]["connections"] = nw["voltage_source"]["source"]["connections"][1:3]
-        end
-    else
-        data_eng["voltage_source"]["source"]["rs"] = data_eng["voltage_source"]["source"]["rs"][1:3,1:3]
-        data_eng["voltage_source"]["source"]["xs"] = data_eng["voltage_source"]["source"]["xs"][1:3,1:3]
-        data_eng["voltage_source"]["source"]["connections"] = data_eng["voltage_source"]["source"]["connections"][1:3]
-    end
-    return nothing
-end
+# ## 3 wire   ---  get these files from  https://github.com/sanderclaeys/DistributionTestCases.jl
+case = "ieee13_pmd"
+# case = "ieee34_pmd"
+# case = "ieee123_pmd"
 
-
-function multinetwork_data_math_correction!(data_math::Dict{String, Any})
-    @assert data_math["multinetwork"]
-    @assert data_math["data_model"]==MATHEMATICAL
-    for (nw, dm) in data_math["nw"]
-        dm["data_model"] = MATHEMATICAL
-        dm["map"] = data_math["map"]
-        dm["bus_lookup"] = data_math["bus_lookup"][nw]
-    end
-    return nothing
-end
-
-
-function sourcebus_voltage_vector_correction!(data_math::Dict{String, Any}; explicit_neutral=true)
-    if haskey(data_math, "multinetwork")
-        for (n,nw) in data_math["nw"]
-            for (i, bus) in data_math["nw"]["bus"]
-                if bus["bus_type"] == 3
-                    if explicit_neutral
-                        bus["vm"] = bus["vm"][1:length(bus["terminals"])]
-                        bus["va"] = bus["va"][1:length(bus["terminals"])]
-                        bus["vmin"] = bus["vmin"][1:length(bus["terminals"])]
-                        bus["vmax"] = bus["vmax"][1:length(bus["terminals"])]
-                        bus["grounded"] = bus["grounded"][1:length(bus["terminals"])]
-                    else
-                        if neutral_idx ∈ bus["terminals"]
-                            bus["terminals"] = bus["terminals"][1:end-1]
-                        end
-                        bus["vm"] = bus["vm"][1:length(bus["terminals"])]
-                        bus["va"] = bus["va"][1:length(bus["terminals"])]
-                        bus["vmin"] = bus["vmin"][1:length(bus["terminals"])]
-                        bus["vmax"] = bus["vmax"][1:length(bus["terminals"])]
-                        bus["grounded"] = bus["grounded"][1:length(bus["terminals"])]
-                    end
-                end
-            end
-        end
-    else
-        for (i, bus) in data_math["bus"]
-            if bus["bus_type"] == 3
-                if explicit_neutral
-                    bus["vm"] = bus["vm"][1:length(bus["terminals"])]
-                    bus["va"] = bus["va"][1:length(bus["terminals"])]
-                    bus["vmin"] = bus["vmin"][1:length(bus["terminals"])]
-                    bus["vmax"] = bus["vmax"][1:length(bus["terminals"])]
-                    bus["grounded"] = bus["grounded"][1:length(bus["terminals"])]
-                else
-                    if neutral_idx ∈ bus["terminals"]
-                        bus["terminals"] = bus["terminals"][1:end-1]
-                    end
-                    bus["vm"] = bus["vm"][1:length(bus["terminals"])]
-                    bus["va"] = bus["va"][1:length(bus["terminals"])]
-                    bus["vmin"] = bus["vmin"][1:length(bus["terminals"])]
-                    bus["vmax"] = bus["vmax"][1:length(bus["terminals"])]
-                    bus["grounded"] = bus["grounded"][1:length(bus["terminals"])]
-                end
-            end
-        end
-    end
-    return nothing
-end
-
-
-
-function update_math_model_3wire!(math)
-    data_math["conductor_ids"] = data_math["conductor_ids"][1:3]
-
-    for (i,bus) in math["bus"]
-        explicit_neutral = false
-        if haskey(bus, "terminals") && neutral_idx ∈ bus["terminals"]
-            explicit_neutral = true
-        end
-
-        if explicit_neutral
-
-            if haskey(bus, "terminals")
-                bus["terminals"] = bus["terminals"][1:end-1]
-            end
-
-            if haskey(bus, "grounded")
-                bus["grounded"] = bus["grounded"][1:end-1]
-            end
-            
-            if haskey(bus, "vmax")
-                bus["vmax"] = bus["vmax"][1:end-1]
-            end
-            if haskey(bus, "vmin")
-                bus["vmin"] = bus["vmin"][1:end-1]
-            end
-            bus["vmin"] = 0.9 * ones(length(bus["terminals"]))
-            bus["vmax"] = 1.1 * ones(length(bus["terminals"]))
-        end
-    end
-
-    for (l,branch) in math["branch"]
-        explicit_neutral = false
-        if haskey(branch, "t_connections") && neutral_idx ∈ branch["t_connections"]
-            explicit_neutral = true
-            branch["t_connections"] = branch["t_connections"][1:end-1]
-        end
-        if haskey(branch, "f_connections") && neutral_idx ∈ branch["f_connections"]
-            explicit_neutral = true
-            branch["f_connections"] = branch["f_connections"][1:end-1]
-        end
-        if haskey(branch, "br_r") && explicit_neutral
-            branch["br_r"] = branch["br_r"][1:end-1,1:end-1]
-        end
-        if haskey(branch, "br_x") && explicit_neutral
-            branch["br_x"] = branch["br_x"][1:end-1,1:end-1]
-        end
-        if haskey(branch, "g_to") && explicit_neutral
-            branch["g_to"] = branch["g_to"][1:end-1,1:end-1]
-        end
-        if haskey(branch, "g_fr") && explicit_neutral
-            branch["g_fr"] = branch["g_fr"][1:end-1,1:end-1]
-        end
-        if haskey(branch, "b_to") && explicit_neutral
-            branch["b_to"] = branch["b_to"][1:end-1,1:end-1]
-        end
-        if haskey(branch, "b_fr") && explicit_neutral
-            branch["b_fr"] = branch["b_fr"][1:end-1,1:end-1]
-        end
-        if haskey(branch, "c_rating_a") && explicit_neutral
-            branch["c_rating_a"] = branch["c_rating_a"][1:end-1]
-        end
-    end
-
-    for (t,transformer) in math["transformer"]
-        if haskey(transformer, "t_connections") && neutral_idx ∈ transformer["t_connections"]
-            transformer["t_connections"] = transformer["t_connections"][1:end-1]
-        end
-        if haskey(transformer, "f_connections") && neutral_idx ∈ transformer["f_connections"]
-            transformer["f_connections"] = transformer["f_connections"][1:end-1]
-        end
-    end
-
-    for (g,gen) in math["gen"]
-        if neutral_idx in gen["connections"]
-            gen["connections"] = gen["connections"][1:end-1]
-            gen["vg"] = gen["vg"][1:end-1]
-            gen["pg"] = gen["pg"][1:end-1]
-            gen["qg"] = gen["qg"][1:end-1]
-            gen["pmax"] = gen["pmax"][1:end-1]
-            gen["pmin"] = gen["pmin"][1:end-1]
-            gen["qmax"] = gen["qmax"][1:end-1]
-            gen["qmin"] = gen["qmin"][1:end-1]
-            gen["cost"] = 1000 .* gen["cost"]
-        end
-    end
-
-
-    for (l,load) in math["load"]
-        if load["configuration"] == WYE && neutral_idx ∈ load["connections"]
-            load["connections"] = load["connections"][1:end-1]
-        end
-    end
-
-    
-    return nothing
-end
-
-
-function find_source_bus_id(math)
-    for (b, bus) in math["bus"]
-        if bus["name"] == "sourcebus"
-            return parse(Int, b)
-        end
-    end
-    return error()
-end
-
-
-
-## 3 wire   ---  get these files from  https://github.com/sanderclaeys/DistributionTestCases.jl
-# case_path = "./examples/IEEE testcases Sander/ieee13_pmd.dss"
-# case_path = "./examples/IEEE testcases Sander/ieee34_pmd.dss"
-case_path = "./examples/IEEE testcases Sander/ieee123_pmd.dss"
-
-data_eng = parse_file(case_path, transformations=[transform_loops!, remove_all_bounds!])
+data_eng = parse_file("$case_path/$case.dss", transformations=[transform_loops!])
 data_eng["is_kron_reduced"] = true
 data_eng["settings"]["sbase_default"] = 1
 vsource_correction_to_3w!(data_eng)
-
 data_math = transform_data_model(data_eng;kron_reduce=false, phase_project=false)
-sourcebus_voltage_vector_correction!(data_math, explicit_neutral=false)
+sourcebus_voltage_vector_correction_3wire!(data_math, explicit_neutral=false)
 update_math_model_3wire!(data_math)
-
 res = PowerModelsDistribution.compute_pf(data_math; explicit_neutral=false)
 
-
-## 4 wire
-case_path = "test/data/en_validation_case_data/test_trans_dy.dss"
-data_eng = parse_file(case_path, transformations=[transform_loops!, remove_all_bounds!])
-vsource_correction_to_4w!(data_eng)
-data_math = transform_data_model(data_eng;kron_reduce=false, phase_project=false)
-res = PowerModelsDistribution.compute_pf(data_math; explicit_neutral=true)
+sol_dss = open("$solution_dir/$case.json", "r") do f
+    JSON.parse(f)
+end
+sol_pmd = transform_solution(res["solution"], data_math, make_si=true)
+v_maxerr_pu = compare_sol_dss_pmd(sol_dss, sol_pmd, data_eng, data_math, verbose=false, compare_math=true)
 
 
-## adapt the script by comparing a 4wire and 3wire testcase -> add_start_voltage!(dm, coordinates=:rectangular, epsilon=0) ???
-case_path = "./test/data/opendss/ut_trans_2w_dy_lag.dss"
-data_eng = parse_file(case_path, transformations=[transform_loops!])
-vsource_correction_to_4w!(data_eng)
-data_math = transform_data_model(data_eng;kron_reduce=false)
-res = PowerModelsDistribution.compute_pf(data_math; explicit_neutral=true)
+## 3wire and 4wire native_pf_testcases
 
-
-## 3wire and 4wire ENWL testcases
-case_path_4w = "./examples/ENWL testcases/four-wire with explicit_neutral"
-cd(case_path_4w)
-data_eng = parse_file("Master.dss", transformations=[transform_loops!])
+case = "test_trans_dy"
+data_eng = parse_file("$case_path/$case.dss", transformations=[transform_loops!])
 vsource_correction_to_4w!(data_eng)
 data_math = transform_data_model(data_eng;kron_reduce=false)
 res = PowerModelsDistribution.compute_pf(data_math; explicit_neutral=true)
 
-cd("../three-wire")
-data_eng = parse_file("Master.dss", transformations=[transform_loops!])
+sol_dss = open("$solution_dir/$case.json", "r") do f
+    JSON.parse(f)
+end
+sol_pmd = transform_solution(res["solution"], data_math, make_si=true)
+v_maxerr_pu = compare_sol_dss_pmd(sol_dss, sol_pmd, data_eng, data_math, verbose=false, compare_math=true)
+
+
+case = "test_trans_yy"
+data_eng = parse_file("$case_path/$case.dss", transformations=[transform_loops!])
+vsource_correction_to_4w!(data_eng)
+data_math = transform_data_model(data_eng;kron_reduce=false)
+res = PowerModelsDistribution.compute_pf(data_math; explicit_neutral=true)
+
+sol_dss = open("$solution_dir/$case.json", "r") do f
+    JSON.parse(f)
+end
+sol_pmd = transform_solution(res["solution"], data_math, make_si=true)
+v_maxerr_pu = compare_sol_dss_pmd(sol_dss, sol_pmd, data_eng, data_math, verbose=false, compare_math=true)
+
+
+
+case = "test_trans_dy_3w"
+data_eng = parse_file("$case_path/$case.dss", transformations=[transform_loops!])
 data_eng["is_kron_reduced"] = true
 vsource_correction_to_3w!(data_eng)
 data_math = transform_data_model(data_eng;kron_reduce=false)
-sourcebus_voltage_vector_correction!(data_math, explicit_neutral=false)
+sourcebus_voltage_vector_correction_3wire!(data_math, explicit_neutral=false)
 update_math_model_3wire!(data_math)
 res = PowerModelsDistribution.compute_pf(data_math; explicit_neutral=false)
+
+sol_dss = open("$solution_dir/$case.json", "r") do f
+    JSON.parse(f)
+end
+sol_pmd = transform_solution(res["solution"], data_math, make_si=true)
+v_maxerr_pu = compare_sol_dss_pmd(sol_dss, sol_pmd, data_eng, data_math, verbose=false, compare_math=true)
+
+
+
+case = "test_trans_yy_3w"
+data_eng = parse_file("$case_path/$case.dss", transformations=[transform_loops!])
+data_eng["is_kron_reduced"] = true
+vsource_correction_to_3w!(data_eng)
+data_math = transform_data_model(data_eng;kron_reduce=false)
+sourcebus_voltage_vector_correction_3wire!(data_math, explicit_neutral=false)
+update_math_model_3wire!(data_math)
+res = PowerModelsDistribution.compute_pf(data_math; explicit_neutral=false)
+
+sol_dss = open("$solution_dir/$case.json", "r") do f
+    JSON.parse(f)
+end
+sol_pmd = transform_solution(res["solution"], data_math, make_si=true)
+v_maxerr_pu = compare_sol_dss_pmd(sol_dss, sol_pmd, data_eng, data_math, verbose=false, compare_math=true)
+
+
+# obtain solution from dss
 
 
 ##
 # run the unit tests with KLU, and with original factorize
 # accessing OpenDSSDirect for results consistency
 # validating the results are consistent with OpenDSS
-# double check what is actually happeind with the aux current and voltage variables in ideal transformers
+# double check what is actually happeing with the aux current and voltage variables in ideal transformers
 # fixing the unit tests
 # double checking KLU settings (default or not?) with respect to OpenDSS settings
 
@@ -279,44 +110,44 @@ res = PowerModelsDistribution.compute_pf(data_math; explicit_neutral=false)
 ## solution builder - > make sure load currents are in dict (phase->cr, ci) format
 
 ##
-data_dir = "test/data/en_validation_case_data"
-cases = [x[1:end-4] for x in readdir(data_dir) if endswith(x, ".dss")]
+# data_dir = "test/data/en_validation_case_data"
+# cases = [x[1:end-4] for x in readdir(data_dir) if endswith(x, ".dss")]
 
-case = "test_load_1ph_delta_cp"
-case_path = "$data_dir/$case.dss"
+# case = "test_load_1ph_delta_cp"
+# case_path = "$data_dir/$case.dss"
 
-data_eng = parse_file(case_path, transformations=[transform_loops!])
-vsource_correction_to_4w!(data_eng)
+# data_eng = parse_file(case_path, transformations=[transform_loops!])
+# vsource_correction_to_4w!(data_eng)
 
-data_math = transform_data_model(data_eng;kron_reduce=false)
+# data_math = transform_data_model(data_eng;kron_reduce=false)
 
-res = compute_pf(data_math; explicit_neutral=true)
+# res = compute_pf(data_math; explicit_neutral=true)
 
-##
+# ##
 
-vm = [v for (i,v) in sort(res["solution"]["bus"]["1"]["vm"])]  # load bus
-va = [v for (i,v) in sort(res["solution"]["bus"]["1"]["va"])]
-v = vm .* exp.(va*im)
+# vm = [v for (i,v) in sort(res["solution"]["bus"]["1"]["vm"])]  # load bus
+# va = [v for (i,v) in sort(res["solution"]["bus"]["1"]["va"])]
+# v = vm .* exp.(va*im)
 
-# c2r = res["solution"]["load"]["2"]["cdr"]
-# c2i = res["solution"]["load"]["2"]["cdi"]
-# c2 = c2r .+ im*c2i
-# S2 = (v[2] - v[3]) * c2[1]'
+# # c2r = res["solution"]["load"]["2"]["cdr"]
+# # c2i = res["solution"]["load"]["2"]["cdi"]
+# # c2 = c2r .+ im*c2i
+# # S2 = (v[2] - v[3]) * c2[1]'
 
-c1r = res["solution"]["load"]["1"]["cdr"]
-c1i = res["solution"]["load"]["1"]["cdi"]
-c1 = c1r .+ im*c1i
-S1 = (v[1] - v[4]) * c1[1]'
+# c1r = res["solution"]["load"]["1"]["cdr"]
+# c1i = res["solution"]["load"]["1"]["cdi"]
+# c1 = c1r .+ im*c1i
+# S1 = (v[1] - v[4]) * c1[1]'
 
 
-##
-vm = [v for (i,v) in sort(res["solution"]["bus"]["3"]["vm"])]  # source bus
-va = [v for (i,v) in sort(res["solution"]["bus"]["3"]["va"])]
-v = vm .* exp.(va*im)
+# ##
+# vm = [v for (i,v) in sort(res["solution"]["bus"]["3"]["vm"])]  # source bus
+# va = [v for (i,v) in sort(res["solution"]["bus"]["3"]["va"])]
+# v = vm .* exp.(va*im)
 
-cr_branch = res["solution"]["branch"]["2"]["cr"][1:4]
-ci_branch = res["solution"]["branch"]["2"]["ci"][1:4]
-c_banch = cr_branch .+ im*ci_branch
+# cr_branch = res["solution"]["branch"]["2"]["cr"][1:4]
+# ci_branch = res["solution"]["branch"]["2"]["ci"][1:4]
+# c_banch = cr_branch .+ im*ci_branch
 
-using LinearAlgebra
-Ssource = diag(v * c_banch')
+# using LinearAlgebra
+# Ssource = diag(v * c_banch')
--- test/en_pf_native_validation.jl
+++ test/en_pf_native_validation.jl
@@ -84,6 +84,20 @@ function vsource_correction_to_4w!(data_eng)
     return nothing
 end
 
+function vsource_correction_to_3w!(data_eng)
+    if haskey(data_eng, "multinetwork")
+        for (n,nw) in data_eng["nw"]
+            nw["voltage_source"]["source"]["rs"] = nw["voltage_source"]["source"]["rs"][1:3,1:3]
+            nw["voltage_source"]["source"]["xs"] = nw["voltage_source"]["source"]["xs"][1:3,1:3]
+            nw["voltage_source"]["source"]["connections"] = nw["voltage_source"]["source"]["connections"][1:3]
+        end
+    else
+        data_eng["voltage_source"]["source"]["rs"] = data_eng["voltage_source"]["source"]["rs"][1:3,1:3]
+        data_eng["voltage_source"]["source"]["xs"] = data_eng["voltage_source"]["source"]["xs"][1:3,1:3]
+        data_eng["voltage_source"]["source"]["connections"] = data_eng["voltage_source"]["source"]["connections"][1:3]
+    end
+    return nothing
+end
 
 
 function multinetwork_data_math_correction!(data_math::Dict{String, Any})
@@ -96,6 +110,154 @@ function multinetwork_data_math_correction!(data_math::Dict{String, Any})
     end
     return nothing
 end
+
+
+function sourcebus_voltage_vector_correction_3wire!(data_math::Dict{String, Any}; explicit_neutral=true)
+    if haskey(data_math, "multinetwork")
+        for (n,nw) in data_math["nw"]
+            for (i, bus) in data_math["nw"]["bus"]
+                if bus["bus_type"] == 3
+                    if explicit_neutral
+                        bus["vm"] = bus["vm"][1:length(bus["terminals"])]
+                        bus["va"] = bus["va"][1:length(bus["terminals"])]
+                        bus["vmin"] = bus["vmin"][1:length(bus["terminals"])]
+                        bus["vmax"] = bus["vmax"][1:length(bus["terminals"])]
+                        bus["grounded"] = bus["grounded"][1:length(bus["terminals"])]
+                    else
+                        if neutral_idx ∈ bus["terminals"]
+                            bus["terminals"] = bus["terminals"][1:end-1]
+                        end
+                        bus["vm"] = bus["vm"][1:length(bus["terminals"])]
+                        bus["va"] = bus["va"][1:length(bus["terminals"])]
+                        bus["vmin"] = bus["vmin"][1:length(bus["terminals"])]
+                        bus["vmax"] = bus["vmax"][1:length(bus["terminals"])]
+                        bus["grounded"] = bus["grounded"][1:length(bus["terminals"])]
+                    end
+                end
+            end
+        end
+    else
+        for (i, bus) in data_math["bus"]
+            if bus["bus_type"] == 3
+                if explicit_neutral
+                    bus["vm"] = bus["vm"][1:length(bus["terminals"])]
+                    bus["va"] = bus["va"][1:length(bus["terminals"])]
+                    bus["vmin"] = bus["vmin"][1:length(bus["terminals"])]
+                    bus["vmax"] = bus["vmax"][1:length(bus["terminals"])]
+                    bus["grounded"] = bus["grounded"][1:length(bus["terminals"])]
+                else
+                    if neutral_idx ∈ bus["terminals"]
+                        bus["terminals"] = bus["terminals"][1:end-1]
+                    end
+                    bus["vm"] = bus["vm"][1:length(bus["terminals"])]
+                    bus["va"] = bus["va"][1:length(bus["terminals"])]
+                    bus["vmin"] = bus["vmin"][1:length(bus["terminals"])]
+                    bus["vmax"] = bus["vmax"][1:length(bus["terminals"])]
+                    bus["grounded"] = bus["grounded"][1:length(bus["terminals"])]
+                end
+            end
+        end
+    end
+    return nothing
+end
+
+
+
+function update_math_model_3wire!(math)
+    data_math["conductor_ids"] = data_math["conductor_ids"][1:3]
+
+    for (i,bus) in math["bus"]
+        explicit_neutral = false
+        if haskey(bus, "terminals") && neutral_idx ∈ bus["terminals"]
+            explicit_neutral = true
+        end
+
+        if explicit_neutral
+
+            if haskey(bus, "terminals")
+                bus["terminals"] = bus["terminals"][1:end-1]
+            end
+
+            if haskey(bus, "grounded")
+                bus["grounded"] = bus["grounded"][1:end-1]
+            end
+            
+            if haskey(bus, "vmax")
+                bus["vmax"] = bus["vmax"][1:end-1]
+            end
+            if haskey(bus, "vmin")
+                bus["vmin"] = bus["vmin"][1:end-1]
+            end
+            bus["vmin"] = 0.9 * ones(length(bus["terminals"]))
+            bus["vmax"] = 1.1 * ones(length(bus["terminals"]))
+        end
+    end
+
+    for (l,branch) in math["branch"]
+        explicit_neutral = false
+        if haskey(branch, "t_connections") && neutral_idx ∈ branch["t_connections"]
+            explicit_neutral = true
+            branch["t_connections"] = branch["t_connections"][1:end-1]
+        end
+        if haskey(branch, "f_connections") && neutral_idx ∈ branch["f_connections"]
+            explicit_neutral = true
+            branch["f_connections"] = branch["f_connections"][1:end-1]
+        end
+        if haskey(branch, "br_r") && explicit_neutral
+            branch["br_r"] = branch["br_r"][1:end-1,1:end-1]
+        end
+        if haskey(branch, "br_x") && explicit_neutral
+            branch["br_x"] = branch["br_x"][1:end-1,1:end-1]
+        end
+        if haskey(branch, "g_to") && explicit_neutral
+            branch["g_to"] = branch["g_to"][1:end-1,1:end-1]
+        end
+        if haskey(branch, "g_fr") && explicit_neutral
+            branch["g_fr"] = branch["g_fr"][1:end-1,1:end-1]
+        end
+        if haskey(branch, "b_to") && explicit_neutral
+            branch["b_to"] = branch["b_to"][1:end-1,1:end-1]
+        end
+        if haskey(branch, "b_fr") && explicit_neutral
+            branch["b_fr"] = branch["b_fr"][1:end-1,1:end-1]
+        end
+        if haskey(branch, "c_rating_a") && explicit_neutral
+            branch["c_rating_a"] = branch["c_rating_a"][1:end-1]
+        end
+    end
+
+    for (t,transformer) in math["transformer"]
+        if haskey(transformer, "t_connections") && neutral_idx ∈ transformer["t_connections"]
+            transformer["t_connections"] = transformer["t_connections"][1:end-1]
+        end
+        if haskey(transformer, "f_connections") && neutral_idx ∈ transformer["f_connections"]
+            transformer["f_connections"] = transformer["f_connections"][1:end-1]
+        end
+    end
+
+    for (g,gen) in math["gen"]
+        if neutral_idx in gen["connections"]
+            gen["connections"] = gen["connections"][1:end-1]
+            gen["vg"] = gen["vg"][1:end-1]
+            gen["pg"] = gen["pg"][1:end-1]
+            gen["qg"] = gen["qg"][1:end-1]
+            gen["pmax"] = gen["pmax"][1:end-1]
+            gen["pmin"] = gen["pmin"][1:end-1]
+            gen["qmax"] = gen["qmax"][1:end-1]
+            gen["qmin"] = gen["qmin"][1:end-1]
+            gen["cost"] = 1000 .* gen["cost"]
+        end
+    end
+
+    for (l,load) in math["load"]
+        if load["configuration"] == WYE && neutral_idx ∈ load["connections"]
+            load["connections"] = load["connections"][1:end-1]
+        end
+    end
+
+    return nothing
+end
+
 
 ##
 # point to data and solution directory
@@ -113,13 +275,21 @@ filter!(e->e≠"test_load_3ph_delta_exp", cases)
 
         @testset "case $case" begin
             case_path = "$data_dir/$case.dss"
-
             data_eng = parse_file(case_path, transformations=[transform_loops!])
-            vsource_correction_to_4w!(data_eng)
+            explicit_neutral = true
 
-            data_math = transform_data_model(data_eng;kron_reduce=false)
-
-            res = compute_pf(data_math; explicit_neutral=true)
+            # if case ∉ ["test_trans_dy_3w", "test_trans_yy_3w"]
+                vsource_correction_to_4w!(data_eng)
+                data_math = transform_data_model(data_eng; kron_reduce=false)
+            # else
+            #     explicit_neutral = false
+            #     data_eng["is_kron_reduced"] = true
+            #     vsource_correction_to_3w!(data_eng)
+            #     data_math = transform_data_model(data_eng; kron_reduce=false)
+            #     sourcebus_voltage_vector_correction_3wire!(data_math, explicit_neutral=false)
+            #     update_math_model_3wire!(data_math)
+            # end
+            res = compute_pf(data_math; explicit_neutral=explicit_neutral)
 
             # obtain solution from dss
             sol_dss = open("$solution_dir/$case.json", "r") do f
