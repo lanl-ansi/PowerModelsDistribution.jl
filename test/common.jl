b7033c16bb713070c847c440a108ce8f5d9ddfb8
David M Fobes <dfobes@lanl.gov>
2023-04-11 11:49:19 -0600

    REF: tests: move en_pf_validation functions to common

--- test/common.jl
+++ test/common.jl
@@ -43,3 +43,86 @@ sd(pm, pmd_data, name) = pd(sol, pmd_data, name)+im*qd(sol, pmd_data, name)
 
 calc_vm_w(result, id) = sqrt.(      result["solution"]["bus"][id]["w"])
 calc_vm_W(result, id) = sqrt.(LinearAlgebra.diag( result["solution"]["bus"][id]["Wr"]))
+
+
+"The solar parsing is a bit off, this method corrects for that."
+function pv1_correction!(data_eng)
+    pv1 = data_eng["solar"]["pv1"]
+    pv1["pg_lb"] = pv1["pg_ub"] = pv1["pg"]
+    pv1["qg_lb"] = pv1["qg_ub"] = pv1["qg"]
+end
+
+
+"""
+The ACR formulation returns non-physical solutions when the neutral voltage is not bounded below.
+This method uses the ODD solution to find valid lower bounds, so the formulation can be validated.
+"""
+function add_neutral_lb_from_soldss(data_eng, sol_dss)
+    de = deepcopy(data_eng)
+    for (id, sol_bus) in sol_dss["bus"]
+        eng_bus = de["bus"][id]
+        ts = eng_bus["terminals"]
+        for t in [t for t in ts if t>3 && t in keys(sol_bus["vm"])]
+            idx = findfirst(ts.==t)
+            eng_bus["vm_lb"] = haskey(eng_bus, "vm_lb") ? eng_bus["vm_lb"] : fill(0.0, length(ts))
+            eng_bus["vm_lb"][idx] = max(eng_bus["vm_lb"][idx], sol_bus["vm"][t]*1E-3*0.9)
+        end
+    end
+    return de
+end
+
+
+"Compares a PMD and OpenDSS solution, and returns the largest difference in voltage profile in per unit."
+function compare_sol_dss_pmd(sol_dss::Dict{String,<:Any}, sol_pmd::Dict{String,<:Any}, data_eng::Dict{String,<:Any}, data_math::Dict{String,<:Any}; compare_math::Bool=false, verbose::Bool=true, floating_buses::Vector=[], skip_buses::Vector=[], v_err_print_tol::Real=1E-6)
+    max_v_err_pu = 0.0
+
+    # voltage base for ENGINEERING buses in [V]
+    vbase = Dict(id=>data_math["bus"]["$ind"]["vbase"]*data_math["settings"]["voltage_scale_factor"] for (id,ind) in data_math["bus_lookup"])
+
+    buses_intersected = intersect(keys(sol_dss["bus"]), keys(sol_pmd["bus"]))
+    for id in setdiff(buses_intersected, skip_buses)
+        pmd_bus = sol_pmd["bus"][id]
+        dss_bus = sol_dss["bus"][id]
+
+        terminals = data_eng["bus"][id]["terminals"]
+        if compare_math
+            ts = filter(x->haskey(dss_bus["vm"], "$x"), terminals)
+            v_dss = [dss_bus["vm"]["$t"]*exp(im*dss_bus["va"]["$t"]) for t in ts]
+            # convert to V instead of usual kV
+            v_pmd = [pmd_bus["vm"][idx]*exp(im*deg2rad(pmd_bus["va"][idx]))*data_eng["settings"]["voltage_scale_factor"] for (idx,t) in enumerate(ts)]
+        else
+            ts = filter(x->haskey(dss_bus["vm"], x), terminals)
+            v_dss = [dss_bus["vm"]["$t"]*exp(im*dss_bus["va"]["$t"]) for t in ts]
+            # convert to V instead of usual kV
+            v_pmd = [(pmd_bus["vr"][idx]+im*pmd_bus["vi"][idx])*data_eng["settings"]["voltage_scale_factor"] for (idx,t) in enumerate(ts)]
+        end
+
+        # convert to pu
+        v_dss_pu = v_dss/vbase[id]
+        v_pmd_pu = v_pmd/vbase[id]
+
+        # convert to diffs if floating
+        N = length(v_dss)
+        if id in floating_buses && N>1
+            v_dss_pu = [v_dss_pu[i]-v_dss_pu[j] for i in 1:N for j in i:N if i!=j]/vbase[id]
+            v_pmd_pu = [v_pmd_pu[i]-v_pmd_pu[j] for i in 1:N for j in i:N if i!=j]/vbase[id]
+            labels = ["$(ts[i])-$(ts[j])" for i in 1:N for j in i:N if i!=j]
+        else
+            labels = string.(ts)
+        end
+
+        for i in eachindex(v_pmd_pu)
+            v_err_pu = abs.(v_dss_pu[i]-v_pmd_pu[i]); max_v_err_pu = max(max_v_err_pu, v_err_pu)
+
+            if v_err_pu>v_err_print_tol && verbose
+                println("terminal $id.$(labels[i])")
+                println("\t |U| dss: $(abs(v_dss_pu[i]))")
+                println("\t     pmd: $(abs(v_pmd_pu[i]))")
+                println("\t  ∠U dss:  $(angle(v_dss_pu[i]))")
+                println("\t     pmd:  $(angle(v_pmd_pu[i]))")
+            end
+        end
+    end
+
+    return max_v_err_pu
+end
