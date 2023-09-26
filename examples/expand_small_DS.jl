00a2f11e0ba9a7457e496edb2dd4b05cffa44b63
asazamzam <a.samir2013@gmail.com>
2023-09-26 11:39:08 -0600

    Adding expansion formulations for AC-Polar formulations and test case on a 3-bus distribution feeder

---
+++ examples/expand_small_DS.jl
@@ -0,0 +1,44 @@
+import CSV
+import JuMP
+import Ipopt
+import Pkg
+
+using DataFrames
+using Xpress
+using Revise
+using Printf
+using Juniper
+using SCIP
+
+using PowerModelsDistribution
+const PMD = PowerModelsDistribution
+
+network_file = "/Users/azamzam/dev/Julia/power-water-expansion/PowerModelsDistribution.jl/test/data/opendss/case3_unbalanced.dss"
+expansion_file = "/Users/azamzam/dev/Julia/power-water-expansion/PowerModelsDistribution.jl/test/data/ne_json/expansion3u.json"
+
+data = parse_file(network_file, data_model = DSS)
+expand_mods = parse_file(expansion_file)
+PMD._IM.update_data!(data, expand_mods)
+
+apply_voltage_bounds!(data, vm_lb=0.97, vm_ub=1.03)
+
+data_mn = make_multinetwork(data)
+
+interval_len = Dict("interval_len"=> 24)
+PMD._IM.update_data!(data_mn, interval_len)
+
+pm_type = ACPUPowerModel
+
+data_mn_math = transform_data_model(data_mn; kron_reduce=true)
+
+pm = instantiate_mc_model(data_mn_math, pm_type, build_mn_mc_mld_multi_scenario_ne)
+expand_cost = objective_ne(pm)
+JuMP.@constraint(pm.model, expand_cost <= 100000)
+objective_mc_min_fuel_cost(pm)
+
+ipopt_solver = optimizer_with_attributes(() -> Ipopt.Optimizer())
+xpress_solver = optimizer_with_attributes(() -> Xpress.Optimizer(DEFAULTALG=2, PRESOLVE=0, logfile = "output.log"))
+juniper_optimizer = optimizer_with_attributes(Juniper.Optimizer, "nl_solver"=>ipopt_solver, "time_limit"=>600)
+scip_optimizer = optimizer_with_attributes(SCIP.Optimizer, "limits/gap"=>0.05)
+
+res = optimize_model!(pm, optimizer=juniper_optimizer)
---
+++ examples/optimize_small_DS.jl
@@ -0,0 +1,31 @@
+import CSV
+import JuMP
+import Ipopt
+import Pkg
+
+using DataFrames
+using Xpress
+using Revise
+using Printf
+
+using PowerModelsDistribution
+const PMD = PowerModelsDistribution
+
+network_file = "test/data/opendss/case3_unbalanced.dss"
+
+data = parse_file(network_file, data_model = DSS)
+
+apply_voltage_bounds!(data, vm_lb=0.97, vm_ub=1.03)
+
+increase_factor = 4
+
+pm_type = ACPUPowerModel
+
+data_math = transform_data_model(data; kron_reduce=true)
+
+pm = instantiate_mc_model(data_math, pm_type, build_mc_opf)
+
+# xpress_solver = optimizer_with_attributes(() -> Xpress.Optimizer(DEFAULTALG=2, PRESOLVE=0, logfile = "output.log"))
+ipopt_solver = optimizer_with_attributes(() -> Ipopt.Optimizer())
+
+res = optimize_model!(pm, optimizer=ipopt_solver)
