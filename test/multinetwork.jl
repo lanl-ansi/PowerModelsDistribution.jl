68f47e71e86323adb60960e68ec37e305fc464ac
John-Boik <john.boik@itron.com>
2024-03-09 19:35:14 -0700

    oltc tests

--- test/multinetwork.jl
+++ test/multinetwork.jl
@@ -26,4 +26,17 @@
             @test all(all(isapprox.(bus["vm_lb"][filter(x->x∉bus["grounded"],bus["terminals"])]/vbases[id], 0.9; atol=1e-6)) for (id,bus) in filter(x->x.first!="sourcebus",nw["bus"]))
         end
     end
+    
+    @testset "solve_mc_opf_oltc" begin
+        result_mn = PowerModelsDistribution.solve_mc_opf_oltc(IEEE13_Feeder_engr, ACPUPowerModel, ipopt_solver)
+        @test result_mn["termination_status"] == LOCALLY_SOLVED
+        
+        @test all(isapprox.(result_mn["solution"]["nw"]["1"]["voltage_source"]["source"]["pg"], [.74423, .79428, .79403; atol=1e-5))
+        @test all(isapprox.(result_mn["solution"]["nw"]["1"]["voltage_source"]["source"]["qg"], [.23829, .20968, .26733; atol=1e-5))
+        @test all(isapprox.(result_mn["solution"]["nw"]["8"]["voltage_source"]["source"]["pg"], [.83028, .87402, .90503; atol=1e-5))
+        @test all(isapprox.(result_mn["solution"]["nw"]["8"]["voltage_source"]["source"]["qg"], [.28470, .22363, .29205; atol=1e-5))
+        
+        @test all(isapprox.(result_mn["solution"]["nw"]["1"]["transformer"]["reg1"]["tap"], [1.02369, 1.01734, 1.02179; atol=1e-5))
+        @test all(isapprox.(result_mn["solution"]["nw"]["8"]["transformer"]["reg1"]["tap"], [1.02699, 1.01953, 1.02387; atol=1e-5))
+    end
 end
