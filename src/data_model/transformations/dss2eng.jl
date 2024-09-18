lanl-ansi/PowerModelsDistribution.jl

commit e5f18cc8028750a5585cefb2247e5b4ac83968b7

FIX: switches should not have length
In ENGINEERING model, switches should be zero-length objects.
Resolved this by applying length/linecode in the dss2eng parse stage.

Resolves #462

src/data_model/transformations/dss2eng.jl
@@ -492,8 +492,28 @@ function _dss2eng_line!(data_eng::Dict{String,<:Any}, data_dss::OpenDssDataModel
         end
 
         if dss_obj["switch"]
-            eng_obj["state"] = CLOSED
-            eng_obj["dispatchable"] = YES
+            eng_obj["state"] = eng_obj["status"] == ENABLED ? CLOSED : OPEN
+            eng_obj["dispatchable"] = YES # default
+            eng_obj["status"] = ENABLED
+
+            if haskey(eng_obj, "linecode")
+                _apply_linecode!(eng_obj, data_eng)
+            end
+            delete!(eng_obj, "linecode")
+
+            # ENGINEERING model switches are zero-length objects
+            for k in ["b_fr", "b_to", "g_fr", "g_to"]
+                if haskey(eng_obj, k)
+                    _admittance_conversion(data_eng, eng_obj, k)
+                end
+            end
+
+            for k in ["rs", "xs"]
+                if haskey(eng_obj, k)
+                    eng_obj[k] = _impedance_conversion(data_eng, eng_obj, k)
+                end
+            end
+            delete!(eng_obj, "length")
 
             _add_eng_obj!(data_eng, "switch", id, eng_obj)
         else

test/opendss.jl
@@ -133,7 +133,7 @@
 
     @testset "opendss parse switch length verify" begin
         @testset "branches with switches" begin
-            @test eng["switch"]["_l4"]["length"] == 0.001
+            @test dss["line"]["_l4"]["length"] == 0.001
             @test !all(get(br, "switch", false) for (_,br) in math["branch"] if !startswith(br["name"],"_virtual_branch.switch"))
         end
     end

commit f8325cafbc71676b48a1e595c8bdbee6f92625a0

FIX: switch admittance adjustment
Bug introduced in e5f18cc8028750a5585cefb2247e5b4ac83968b7 which seems to only affect numerics on windows/linux, but was incorrectly using _admittance_conversion instead of just multiplying by length

src/data_model/transformations/dss2eng.jl
@@ -502,15 +502,9 @@ function _dss2eng_line!(data_eng::Dict{String,<:Any}, data_dss::OpenDssDataModel
             delete!(eng_obj, "linecode")
 
             # ENGINEERING model switches are zero-length objects
-            for k in ["b_fr", "b_to", "g_fr", "g_to"]
+            for k in ["b_fr", "b_to", "g_fr", "g_to", "rs", "xs"]
                 if haskey(eng_obj, k)
-                    _admittance_conversion(data_eng, eng_obj, k)
-                end
-            end
-
-            for k in ["rs", "xs"]
-                if haskey(eng_obj, k)
-                    eng_obj[k] = _impedance_conversion(data_eng, eng_obj, k)
+                    eng_obj[k] .*= get(eng_obj, length, 1.0)
                 end
             end
             delete!(eng_obj, "length")

test/multinetwork.jl
@@ -30,12 +30,12 @@
     @testset "solve_mc_opf_oltc" begin
         result_mn = PowerModelsDistribution.solve_mn_mc_opf_oltc(IEEE13_Feeder_engr, ACPUPowerModel, ipopt_solver)
         @test result_mn["termination_status"] == LOCALLY_SOLVED
-        
+
         @test all(isapprox.(result_mn["solution"]["nw"]["1"]["voltage_source"]["source"]["pg"], [738.58786, 788.38272, 787.79729]; atol=1e-5))
         @test all(isapprox.(result_mn["solution"]["nw"]["1"]["voltage_source"]["source"]["qg"], [237.68517, 209.61208, 266.77223]; atol=1e-5))
         @test all(isapprox.(result_mn["solution"]["nw"]["8"]["voltage_source"]["source"]["pg"], [847.77707, 889.87745, 918.34146]; atol=1e-5))
         @test all(isapprox.(result_mn["solution"]["nw"]["8"]["voltage_source"]["source"]["qg"], [284.46267, 227.28860, 292.33564]; atol=1e-5))
-        
+
         @test all(isapprox.(result_mn["solution"]["nw"]["1"]["transformer"]["reg1"]["tap"][2], [1.02358, 1.01724, 1.02169]; atol=1e-5))
         @test all(isapprox.(result_mn["solution"]["nw"]["8"]["transformer"]["reg1"]["tap"][2], [1.02719, 1.01984, 1.02414]; atol=1e-5))
     end

test/test_cases.jl
@@ -66,5 +66,5 @@ test_trans_dy = parse_file("../test/data/en_validation_case_data/test_trans_dy.d
 dist_transformer = parse_file("../test/data/opendss/dist_transformer.dss")
 
 # IEEE13 Feeder
-IEEE13_Feeder_engr = parse_file("../test/data/opendss/ieee13_feeder.dss", multinetwork=true, 
+IEEE13_Feeder_engr = parse_file("../test/data/opendss/ieee13_feeder.dss", multinetwork=true,
     time_series="daily", transformations=[remove_line_limits!, remove_transformer_limits!])